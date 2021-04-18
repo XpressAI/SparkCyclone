491f202809b8674a4f25304ea032356067d249db
WGipfeli <w@gipfeli.sg>
2021-04-18 02:20:29 +0100

    Create some basic test-cases for the plug-in and planners.
    Here, we work through, document & get familiar with the relevant Spark's extension interfaces (because Spark is very big, we need to focus on very specific parts of it to make headway) and create dummy implementations of plan rewriters.
    
    The first dummy implementation extracts a simple execution plan and the corresponding unit test pattern-matches against this execution plan to document the key important bits for reference from this unit test file.
    
    The second dummy implementation rewrites the execution plan fully to a stubbed one that returns a constant result.
    
    For the stubbed one, we create 2 unit tests:
    - One that is passing because of the constant result matching the sum of the inputs
    - One that is failing because of the constant result not matching the sum of the inputs
    
    This TDD approach will lead us to develop the right plan rewriter, which will then interface with the Vector Engine. The strategic approach here is to do the necessary bottom up bits from the Spark side, and then the necessary bottom up bits from the Vector Engine side, and do the minimum necessary work to make some basic tests pass.
    
    As part of the plan generation we will generate some sort of mini-program that can be sent to the VE code to do all the computation right there and then. However, details of this are yet to be worked out.

--- aurora4spark-parent/aurora4spark-sql-plugin/src/test/scala/com/nec/spark/SqlPluginTest.scala
+++ aurora4spark-parent/aurora4spark-sql-plugin/src/test/scala/com/nec/spark/SqlPluginTest.scala
@@ -1,10 +1,14 @@
 package com.nec.spark
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.execution.LocalTableScanExec
+import org.apache.spark.sql.execution.aggregate.HashAggregateExec
 import org.apache.spark.{SparkConf, SparkContext}
 import org.scalatest.freespec.AnyFreeSpec
 
+object SqlPluginTest {}
 final class SqlPluginTest extends AnyFreeSpec {
+
   "It is not launched if not specified" in {
     val conf = new SparkConf()
     conf.setMaster("local")
@@ -48,4 +52,152 @@ final class SqlPluginTest extends AnyFreeSpec {
       assert(result == List(3))
     } finally sparkSession.close()
   }
+
+  "We can get an execution plan for a sum of rows and dissect the programmatic structure" in {
+    info(
+      """
+        |We do this so that we know exactly what we need to optimize/rewrite for GPU processing
+        |
+        |We will get there more quickly by doing this sort of use case based reverse engineering.
+        |""".stripMargin
+    )
+    val conf = new SparkConf()
+    conf.setMaster("local")
+    conf.set("spark.ui.enabled", "false")
+    conf.set(
+      "spark.sql.extensions",
+      classOf[SparkPlanSavingPlugin].getCanonicalName
+    )
+    conf.setAppName("local-test")
+    val sparkSession = SparkSession.builder().config(conf).getOrCreate()
+    try {
+      import sparkSession.implicits._
+      Seq(1, 2, 3)
+        .toDS()
+        .createOrReplaceTempView("nums")
+
+      val result =
+        sparkSession.sql("SELECT SUM(value) FROM nums").as[BigDecimal].head()
+
+      assert(
+        SparkPlanSavingPlugin.savedSparkPlan.getClass.getCanonicalName == "org.apache.spark.sql.execution.aggregate.HashAggregateExec"
+      )
+      SparkPlanSavingPlugin.savedSparkPlan match {
+        case first @ HashAggregateExec(
+              requiredChildDistributionExpressions,
+              groupingExpressions,
+              aggregateExpressions,
+              aggregateAttributes,
+              initialInputBufferOffset,
+              resultExpressions,
+              child
+            ) =>
+          info(s"First root of the plan: ${first}")
+          assert(
+            child.getClass.getCanonicalName == "org.apache.spark.sql.execution.exchange.ShuffleExchangeExec"
+          )
+          child match {
+            case second @ org.apache.spark.sql.execution.exchange
+                  .ShuffleExchangeExec(
+                    outputPartitioning,
+                    child2,
+                    shuffleOrigin
+                  ) =>
+              info(s"Second root of the plan: ${second}")
+              assert(
+                child2.getClass.getCanonicalName == "org.apache.spark.sql.execution.aggregate.HashAggregateExec"
+              )
+              child2 match {
+                case third @ org.apache.spark.sql.execution.aggregate
+                      .HashAggregateExec(
+                        requiredChildDistributionExpressions,
+                        groupingExpressions,
+                        aggregateExpressions,
+                        aggregateAttributes,
+                        initialInputBufferOffset,
+                        resultExpressions,
+                        child3
+                      ) =>
+                  info(s"Third root of the plan: ${third}")
+                  assert(
+                    child3.getClass.getCanonicalName == "org.apache.spark.sql.execution.LocalTableScanExec"
+                  )
+                  child3 match {
+                    case fourth @ LocalTableScanExec(output, rows) =>
+                      info(s"Fourth root of the plan: ${fourth}")
+                      assert(output.length == 1, "There is only 1 column")
+                      assert(rows.length == 3, "There are 3 rows")
+                  }
+              }
+          }
+      }
+
+      assert(result == BigDecimal(6))
+    } finally sparkSession.close()
+  }
+
+  "We can do a pretend plug-in that will return 6" in {
+    info(
+      """
+      This enables us to be confident this is the right place to do the rewriting.
+      The significance of this is that from here, we will be able to compare expected results with actual results after we apply the plug-in
+      """
+    )
+    val conf = new SparkConf()
+    conf.setMaster("local")
+    conf.set("spark.ui.enabled", "false")
+    conf.set(
+      "spark.sql.extensions",
+      classOf[DummyShortCircuitSqlPlugin].getCanonicalName
+    )
+    conf.setAppName("local-test")
+    val sparkSession = SparkSession.builder().config(conf).getOrCreate()
+    try {
+      import sparkSession.implicits._
+
+      DummyShortCircuitSqlPlugin.applyShortCircuit = false
+      Seq(1, 2, 3)
+        .toDS()
+        .createOrReplaceTempView("nums")
+      val sumDataSet =
+        sparkSession.sql("SELECT SUM(value) FROM nums").as[BigDecimal]
+
+      DummyShortCircuitSqlPlugin.applyShortCircuit = true
+      sumDataSet.explain(true)
+      val result = sumDataSet.head()
+
+      assert(result == BigDecimal(6))
+    } finally sparkSession.close()
+  }
+
+  /** this test is ignored because it's currently failing as our computation engine returns only a static/stubbed value. */
+  "We can do a pretend plug-in that will return 6, however it should return 10" ignore {
+    val conf = new SparkConf()
+    conf.setMaster("local")
+    conf.set("spark.ui.enabled", "false")
+    conf.set(
+      "spark.sql.extensions",
+      classOf[DummyShortCircuitSqlPlugin].getCanonicalName
+    )
+    conf.setAppName("local-test")
+    val sparkSession = SparkSession.builder().config(conf).getOrCreate()
+    try {
+      import sparkSession.implicits._
+      DummyShortCircuitSqlPlugin.applyShortCircuit = false
+
+      Seq(1, 2, 3, 4)
+        .toDS()
+        .createOrReplaceTempView("nums")
+
+      val sumDataSet =
+        sparkSession.sql("SELECT SUM(value) FROM nums").as[BigDecimal]
+
+      DummyShortCircuitSqlPlugin.applyShortCircuit = true
+      sumDataSet.explain(true)
+      val result = sumDataSet.head()
+
+      assert(result == BigDecimal(10))
+    } finally sparkSession.close()
+  }
+
 }
