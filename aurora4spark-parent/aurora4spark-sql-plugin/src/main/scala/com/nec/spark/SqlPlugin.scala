9894b202d7fb6ec3eb21c5ced42d57f6e030799e
Wosin <blind.van@gmail.com>
2021-04-20 20:32:50 +0200

    Add LoggingExecutor plugin.

--- aurora4spark-parent/aurora4spark-sql-plugin/src/main/scala/com/nec/spark/SqlPlugin.scala
+++ aurora4spark-parent/aurora4spark-sql-plugin/src/main/scala/com/nec/spark/SqlPlugin.scala
@@ -1,21 +1,10 @@
 package com.nec.spark
 
-import org.apache.spark.api.plugin.{
-  DriverPlugin,
-  ExecutorPlugin,
-  PluginContext,
-  SparkPlugin
-}
+import org.apache.spark.api.plugin.{DriverPlugin, ExecutorPlugin, SparkPlugin}
 import org.apache.spark.internal.Logging
 
 class SqlPlugin extends SparkPlugin with Logging {
   override def driverPlugin(): DriverPlugin = new Aurora4SparkDriver()
-  override def executorPlugin(): ExecutorPlugin = null
-}
 
-class Aurora4SparkExecutor extends ExecutorPlugin with Logging {
-  override def init(
-      ctx: PluginContext,
-      extraConf: java.util.Map[String, String]
-  ): Unit = null
+  override def executorPlugin(): ExecutorPlugin = new Aurora4SparkExecutorPlugin()
 }
