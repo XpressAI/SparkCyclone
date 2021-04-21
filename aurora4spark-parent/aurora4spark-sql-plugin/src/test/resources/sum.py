34b0804445edea56dcc6359b04f7a9e8b23ea0c3
Wosin <blind.van@gmail.com>
2021-04-22 00:36:36 +0200

    Change python script to allow removing C file.

--- aurora4spark-parent/aurora4spark-sql-plugin/src/test/resources/sum.py
+++ aurora4spark-parent/aurora4spark-sql-plugin/src/test/resources/sum.py
@@ -3,9 +3,26 @@ import os
 import sys
 
 nodeid = os.environ.get("VE_NODE_NUMBER", 0)
+bld = veo.VeBuild()
+bld.set_build_dir("_ve_build")
+bld.set_c_src("_sum", r"""
+
+double sum(double *a, int n)
+{
+    int i;
+    double sum = 0;
+    for (i = 0; i < n; i++) {
+        sum += a[i];
+    }
+
+    return sum;
+}""")
+
+ve_so_name = bld.build_so()
+
 proc = veo.VeoAlloc().proc
 ctxt = proc.open_context()
-lib = proc.load_library((os.getcwd() + "/sum.so").encode("UTF-8"))
+lib = proc.load_library((os.getcwd() + "/" + ve_so_name).encode("UTF-8"))
 lib.sum.args_type(b"double*", "int")
 lib.sum.ret_type("double")
 np = veo.np
@@ -14,7 +31,7 @@ numbers = []
 
 for arg in sys.argv[1:]:
     try:
-        number =float(arg)
+        number = float(arg)
         numbers.append(number)
     except:
         print("Provided argument is not a number {}".format(arg))
