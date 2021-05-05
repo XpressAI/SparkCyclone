14580cd84be6c1acb2349bc85c8ab325575436cd
Wosin <blind.van@gmail.com>
2021-05-05 13:31:12 +0200

    Add AvgSimple.

---
+++ aurora4spark-parent/aurora4spark-sql-plugin/ve-direct/src/main/scala/com/nec/AvgSimple.scala
@@ -0,0 +1,42 @@
+package com.nec
+
+import com.nec.VeFunction.StackArgument
+
+object AvgSimple {
+  val C_Definition =
+    """
+      |double avg(double *a, int n)
+      |{
+      |    int i;
+      |    double sum = 0;
+      |    for (i = 0; i < n; i++) {
+      |        sum += a[i];
+      |    }
+      |
+      |    return sum/i;
+      |}
+      |""".stripMargin
+
+  val Ve_F = VeFunction(
+    name = "avg",
+    args = List[StackArgument](StackArgument.ListOfDouble, StackArgument.Int),
+    ret_type = Some("'double'")
+  )
+
+  def avgSimple(veCallContext: VeCallContext, inputs: List[Double]): Double = {
+    veCallContext.execute(
+      veFunction = Ve_F,
+      ln = inputs.length,
+      uploadData = { poss =>
+        inputs.iterator.zipWithIndex.foreach { case (a, idx) =>
+          poss.args(0).foreach { Pos_In_1 =>
+            veCallContext.unsafe.putDouble(Pos_In_1 + idx * 8, a)
+          }
+        }
+      },
+      loadData = { (ret, poss) =>
+        ret.as[Double]
+      }
+    )
+  }
+}
