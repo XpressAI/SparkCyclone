64ef736d8eb24a0ac11b085b5614615b93b7d62c
Wosin <blind.van@gmail.com>
2021-05-07 00:15:47 +0200

    Fixed according to comments.

--- aurora4spark-parent/aurora4spark-sql-plugin/ve-direct/src/main/scala/com/nec/SumMultipleColumns.scala
+++ aurora4spark-parent/aurora4spark-sql-plugin/ve-direct/src/main/scala/com/nec/SumMultipleColumns.scala
@@ -14,7 +14,7 @@ object SumMultipleColumns {
                        |    for (i = 0; i < n; i++) {
                        |        double sum = 0;
                        |        for(j = 0; j< m; j++){
-                       |           sum += a[(i*n) + j];
+                       |           sum += a[(i*m) + j];
                        |        }
                        |        b[i] = sum;
                        |    }
--- aurora4spark-parent/aurora4spark-sql-plugin/ve-direct/src/main/scala/com/nec/VeDirectApp.scala
+++ aurora4spark-parent/aurora4spark-sql-plugin/ve-direct/src/main/scala/com/nec/VeDirectApp.scala
@@ -59,16 +59,22 @@ object VeDirectApp {
         val vej = new VeJavaContext(ctx, lib)
         println(SumSimple.sum_doubles(vej, List(1, 2, 3, 4)))
         println(AvgSimple.avg_doubles(vej, List(1, 2, 3, 10)))
-        println(SumMultipleColumns.sum_multiple_doubles(vej, List(
+        val multiColumnSumResult = SumMultipleColumns.sum_multiple_doubles(vej, List(
           List(1,2,3),
           List(2,3,4),
-          List(5,4,3)
-        )))
-        println(AvgMultipleColumns.avg_multiple_doubles(vej, List(
+          List(5,4,3),
+          List(10,10,10)
+        ))
+        println(multiColumnSumResult)
+        assert(multiColumnSumResult == List(6.0, 9.0, 12.0, 30.0))
+        val multiColumnAvgResult = AvgMultipleColumns.avg_multiple_doubles(vej, List(
           List(5, 10, 15),
           List(3, 27, 30),
-          List(100, 200, 300)
-        )))
+          List(100, 200, 300),
+          List(1000, 2000, 3000)
+        ))
+        println(multiColumnAvgResult)
+        assert(multiColumnAvgResult== List(10.0, 20.0 , 200.0 ,2000.0))
         println(
           SumPairwise.pairwise_sum_doubles(vej, List[(Double, Double)]((1, 1), (1, 2), (2, 9)))
         )
