4d43bd51b50dfe9a6b6561ad66a78271ad1896e4
William <52247468+wgipfeli@users.noreply.github.com>
2021-06-06 22:47:15 +0100

    Move C-dependant tests to CMake scope (#71)

--- aurora4spark-parent/aurora4spark-sql-plugin/src/test/scala/com/nec/spark/agile/SparkSanityTests.scala
+++ aurora4spark-parent/aurora4spark-sql-plugin/src/test/scala/com/nec/spark/agile/SparkSanityTests.scala
@@ -1,17 +1,13 @@
 package com.nec.spark.agile
 
-import com.nec.arrow.CArrowNativeInterfaceNumeric
-import com.nec.cmake.CMakeBuilder
-import com.nec.spark.SampleTestData.{SampleMultiColumnCSV, SampleTwoColumnParquet}
-import com.nec.spark.planning.ArrowSummingPlan.ArrowSummer.CBased
-import com.nec.spark.{Aurora4SparkDriver, Aurora4SparkExecutorPlugin, SparkAdditions}
-import com.nec.spark.planning.{AddPlanExtractor, ArrowAveragingPlan, ArrowSummingPlan, SingleColumnAvgPlanExtractor, SingleColumnSumPlanExtractor, SparkSqlPlanExtension}
+import com.nec.spark.Aurora4SparkDriver
+import com.nec.spark.Aurora4SparkExecutorPlugin
+import com.nec.spark.SparkAdditions
+import com.nec.spark.planning.SingleColumnSumPlanExtractor
 import org.scalatest.BeforeAndAfter
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
-
-import org.apache.spark.sql.internal.SQLConf.{COLUMN_VECTOR_OFFHEAP_ENABLED, WHOLESTAGE_CODEGEN_ENABLED}
-import org.apache.spark.sql.types.{DoubleType, StructField, StructType}
+import org.apache.spark.sql.internal.SQLConf.WHOLESTAGE_CODEGEN_ENABLED
 
 /**
  * These tests are to get familiar with Spark and encode any oddities about it.
@@ -59,139 +55,6 @@ final class SparkSanityTests
         .map(_.collect())
         .map(_.map(_.getDouble(0)).toList) == Some(List(1, 2, 3))
     )
-  }
-
-  "We Pairwise-add off the heap" in withSparkSession(
-    _.set("spark.sql.extensions", classOf[SparkSqlPlanExtension].getCanonicalName)
-      .set(COLUMN_VECTOR_OFFHEAP_ENABLED.key, "true")
-  ) { sparkSession =>
-    import sparkSession.implicits._
-
-    SparkSqlPlanExtension.rulesToApply.clear()
-
-    SparkSqlPlanExtension.rulesToApply.append { sparkPlan =>
-      AddPlanExtractor
-        .matchAddPairwisePlan(
-          sparkPlan,
-          new CArrowNativeInterfaceNumeric(CMakeBuilder.CLibPath.toString)
-        )
-        .getOrElse(sys.error(s"Plan was not matched: ${sparkPlan}"))
-    }
-
-    val csvSchema = StructType(
-      Seq(
-        StructField("a", DoubleType, nullable = false),
-        StructField("b", DoubleType, nullable = false)
-      )
-    )
-    val sumDataSet2 = sparkSession.read
-      .format("csv")
-      .schema(csvSchema)
-      .load(SampleMultiColumnCSV.toString)
-      .as[(Double, Double)]
-      .selectExpr("a + b")
-      .as[Double]
-      .debugConditionally()
-
-    val listOfDoubles = sumDataSet2.collect().toList
-    assert(listOfDoubles == List(3, 5, 7, 9, 58))
-  }
-
-  "We Pairwise-add Parquet off the heap" in withSparkSession(
-    _.set("spark.sql.extensions", classOf[SparkSqlPlanExtension].getCanonicalName)
-      .set(COLUMN_VECTOR_OFFHEAP_ENABLED.key, "true")
-  ) { sparkSession =>
-    import sparkSession.implicits._
-
-    SparkSqlPlanExtension.rulesToApply.clear()
-
-    SparkSqlPlanExtension.rulesToApply.append { sparkPlan =>
-      AddPlanExtractor
-        .matchAddPairwisePlan(
-          sparkPlan,
-          new CArrowNativeInterfaceNumeric(CMakeBuilder.CLibPath.toString)
-        )
-        .getOrElse(sys.error(s"Plan was not matched: ${sparkPlan}"))
-    }
-
-    val sumDataSet2 = sparkSession.read
-      .format("parquet")
-      .load(SampleTwoColumnParquet.toString)
-      .as[(Double, Double)]
-      .selectExpr("a + b")
-      .as[Double]
-      .debugConditionally()
-
-    val listOfDoubles = sumDataSet2.collect().toList
-    assert(listOfDoubles == List(3, 5, 7, 9, 58))
-  }
-
-  "We handle single column average with specific plan" in withSparkSession(
-    _.set("spark.sql.extensions", classOf[SparkSqlPlanExtension].getCanonicalName)
-      .set(COLUMN_VECTOR_OFFHEAP_ENABLED.key, "true")
-  ) { sparkSession =>
-    import sparkSession.implicits._
-
-    SparkSqlPlanExtension.rulesToApply.clear()
-
-    SparkSqlPlanExtension.rulesToApply.append { sparkPlan =>
-      SingleColumnAvgPlanExtractor
-        .matchPlan(sparkPlan)
-        .map(plan =>
-          new ArrowAveragingPlan(
-            plan.sparkPlan,
-            CBased(CMakeBuilder.CLibPath.toString),
-            plan.column
-          )
-        )
-        .getOrElse(sys.error(s"Plan was not matched: ${sparkPlan}"))
-    }
-
-    val inputData: Seq[(Double, Double)] = Seq((10, 2), (20, 5), (30, 6), (40, 7))
-
-    val sumDataSet2 = inputData
-      .toDS()
-      .as[(Double, Double)]
-      .selectExpr("AVG(_1)")
-      .as[Double]
-      .debugConditionally()
-
-    val listOfDoubles = sumDataSet2.collect().toList
-    assert(listOfDoubles == List(25))
-  }
-
-  "We handle single column sum with specific plan" in withSparkSession(
-    _.set("spark.sql.extensions", classOf[SparkSqlPlanExtension].getCanonicalName)
-      .set(COLUMN_VECTOR_OFFHEAP_ENABLED.key, "true")
-  ) { sparkSession =>
-    import sparkSession.implicits._
-
-    SparkSqlPlanExtension.rulesToApply.clear()
-
-    SparkSqlPlanExtension.rulesToApply.append { sparkPlan =>
-      SingleColumnSumPlanExtractor
-        .matchPlan(sparkPlan)
-        .map(plan =>
-          ArrowSummingPlan(
-            plan.sparkPlan,
-            CBased(CMakeBuilder.CLibPath.toString),
-            plan.column
-          )
-        )
-        .getOrElse(sys.error(s"Plan was not matched: ${sparkPlan}"))
-    }
-
-    val inputData: Seq[(Double, Double)] = Seq((10, 2), (20, 5), (30, 6), (40, 7))
-
-    val sumDataSet2 = inputData
-      .toDS()
-      .as[(Double, Double)]
-      .selectExpr("SUM(_1)")
-      .as[Double]
-      .debugConditionally()
-
-    val listOfDoubles = sumDataSet2.collect().toList
-    assert(listOfDoubles == List(100.0))
   }
 
 }
--- aurora4spark-parent/aurora4spark-sql-plugin/src/test/scala/com/nec/spark/planning/AveragingSparkPlanSpec.scala
+++ aurora4spark-parent/aurora4spark-sql-plugin/src/test/scala/com/nec/spark/planning/AveragingSparkPlanSpec.scala
@@ -1,15 +1,9 @@
 package com.nec.spark.planning
-import com.nec.cmake.CMakeBuilder
 import com.nec.spark.SparkAdditions
-import com.nec.spark.agile.{OutputColumn, OutputColumnPlanDescription}
-import com.nec.spark.planning.ArrowSummingPlan.ArrowSummer.CBased
 import org.scalatest.BeforeAndAfter
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
-import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
-
-import org.apache.spark.sql.execution.RowToColumnarExec
-import org.apache.spark.sql.internal.SQLConf.{COLUMN_VECTOR_OFFHEAP_ENABLED, WHOLESTAGE_CODEGEN_ENABLED}
+import org.apache.spark.sql.internal.SQLConf.WHOLESTAGE_CODEGEN_ENABLED
 
 final class AveragingSparkPlanSpec
   extends AnyFreeSpec
@@ -28,7 +22,10 @@ final class AveragingSparkPlanSpec
     val executionPlan =
       sparkSession.sql("SELECT AVG(value)  FROM nums").as[(Double)].executionPlan
 
-    assert(SingleColumnAvgPlanExtractor.matchPlan(executionPlan).isDefined, executionPlan.toString())
+    assert(
+      SingleColumnAvgPlanExtractor.matchPlan(executionPlan).isDefined,
+      executionPlan.toString()
+    )
   }
 
   "Specific plugin does not match average of sum" in withSparkSession(
@@ -42,194 +39,9 @@ final class AveragingSparkPlanSpec
     val executionPlan =
       sparkSession.sql("SELECT AVG(value + value) FROM nums").as[(Double)].executionPlan
 
-    assert(SingleColumnAvgPlanExtractor.matchPlan(executionPlan).isDefined, executionPlan.toString())
+    assert(
+      SingleColumnAvgPlanExtractor.matchPlan(executionPlan).isDefined,
+      executionPlan.toString()
+    )
   }
-
-  "We extract data with RowToColumnarExec" in withSparkSession(
-    _.set("spark.sql.extensions", classOf[SparkSqlPlanExtension].getCanonicalName)
-      .set(COLUMN_VECTOR_OFFHEAP_ENABLED.key, "true")
-  ) { sparkSession =>
-    import sparkSession.implicits._
-
-    val nums = List[Double](1, 2, 3, 4, Math.abs(scala.util.Random.nextInt() % 200))
-
-    SparkSqlPlanExtension.rulesToApply.clear()
-
-    nums
-      .toDS()
-      .createOrReplaceTempView("nums")
-
-    SparkSqlPlanExtension.rulesToApply.append { sparkPlan =>
-      SingleColumnAvgPlanExtractor
-        .matchPlan(sparkPlan)
-        .map { childPlan =>
-          ArrowAveragingPlan(
-            RowToColumnarExec(childPlan.sparkPlan),
-            CBased(CMakeBuilder.CLibPath.toString),
-            childPlan.column
-          )
-        }
-        .getOrElse(fail("Not expected to be here"))
-    }
-
-    val sumDataSet =
-      sparkSession
-        .sql("SELECT AVG(value) FROM nums")
-        .as[Double]
-        .debugConditionally()
-
-    val listOfDoubles = sumDataSet.collect().toList
-    info(listOfDoubles.toString())
-
-    val result = listOfDoubles.head
-    assert(result == nums.sum / nums.length)
-  }
-
-  "We can average multiple columns separately off the heap" in withSparkSession(
-    _.set("spark.sql.extensions", classOf[SparkSqlPlanExtension].getCanonicalName)
-      .set(COLUMN_VECTOR_OFFHEAP_ENABLED.key, "true")
-  ) { sparkSession =>
-    import sparkSession.implicits._
-
-    val nums = List[(Double, Double, Double)]((1, 2, 3), (1, 5, 3), (10, 20, 30))
-
-    SparkSqlPlanExtension.rulesToApply.clear()
-
-    nums
-      .toDS()
-      .createOrReplaceTempView("nums")
-
-    SparkSqlPlanExtension.rulesToApply.append { sparkPlan =>
-      VeoGenericPlanExtractor
-        .matchPlan(sparkPlan)
-        .map { childPlan =>
-          ArrowGenericAggregationPlanOffHeap(
-            RowToColumnarExec(childPlan.sparkPlan),
-            childPlan.outColumns.map {
-              case OutputColumnPlanDescription(
-                    inputColumns,
-                    outputColumnIndex,
-                    columnAggregation,
-                    outputAggregator
-                  ) =>
-                OutputColumn(
-                  inputColumns,
-                  outputColumnIndex,
-                  createUnsafeColumnAggregator(columnAggregation),
-                  createUnsafeAggregator(outputAggregator)
-                )
-            }
-          )
-        }
-        .getOrElse(fail("Not expected to be here"))
-    }
-
-    val sumDataSet =
-      sparkSession
-        .sql("SELECT AVG(nums._1), AVG(nums._2), AVG(nums._3) FROM nums")
-        .as[(Double, Double, Double)]
-        .debugConditionally()
-
-    val listOfDoubles = sumDataSet.collect().head
-    assert(listOfDoubles == (4.0, 9.0, 12.0))
-  }
-
-  "We can average added columns" in withSparkSession(
-    _.set("spark.sql.extensions", classOf[SparkSqlPlanExtension].getCanonicalName)
-      .set(COLUMN_VECTOR_OFFHEAP_ENABLED.key, "true")
-  ) { sparkSession =>
-    import sparkSession.implicits._
-
-    val nums = List[(Double, Double, Double)]((1, 2, 3), (1, 5, 3), (10, 20, 30))
-
-    SparkSqlPlanExtension.rulesToApply.clear()
-
-    nums
-      .toDS()
-      .createOrReplaceTempView("nums")
-
-    SparkSqlPlanExtension.rulesToApply.append { sparkPlan =>
-      VeoGenericPlanExtractor
-        .matchPlan(sparkPlan)
-        .map { childPlan =>
-          ArrowGenericAggregationPlanOffHeap(
-            childPlan.sparkPlan,
-            childPlan.outColumns.map {
-              case OutputColumnPlanDescription(
-                    inputColumns,
-                    outputColumnIndex,
-                    columnAggregation,
-                    outputAggregator
-                  ) =>
-                OutputColumn(
-                  inputColumns,
-                  outputColumnIndex,
-                  createUnsafeColumnAggregator(columnAggregation),
-                  createUnsafeAggregator(outputAggregator)
-                )
-            }
-          )
-        }
-        .getOrElse(fail("Not expected to be here"))
-    }
-
-    val sumDataSet =
-      sparkSession
-        .sql("SELECT AVG(nums._1 + nums._2 + nums._3) FROM nums")
-        .as[Double]
-        .debugConditionally()
-
-    val listOfDoubles = sumDataSet.collect().head
-    assert(listOfDoubles == 25.0)
-  }
-
-  "We can average subtracted columns" in withSparkSession(
-    _.set("spark.sql.extensions", classOf[SparkSqlPlanExtension].getCanonicalName)
-      .set(COLUMN_VECTOR_OFFHEAP_ENABLED.key, "true")
-  ) { sparkSession =>
-    import sparkSession.implicits._
-
-    val nums = List[(Double, Double, Double)]((1, 2, 6), (1, 4, 11), (10, 20, 33))
-
-    SparkSqlPlanExtension.rulesToApply.clear()
-
-    nums
-      .toDS()
-      .createOrReplaceTempView("nums")
-
-    SparkSqlPlanExtension.rulesToApply.append { sparkPlan =>
-      VeoGenericPlanExtractor
-        .matchPlan(sparkPlan)
-        .map { childPlan =>
-          ArrowGenericAggregationPlanOffHeap(
-            RowToColumnarExec(childPlan.sparkPlan),
-            childPlan.outColumns.map {
-              case OutputColumnPlanDescription(
-                    inputColumns,
-                    outputColumnIndex,
-                    columnAggregation,
-                    outputAggregator
-                  ) =>
-                OutputColumn(
-                  inputColumns,
-                  outputColumnIndex,
-                  createUnsafeColumnAggregator(columnAggregation),
-                  createUnsafeAggregator(outputAggregator)
-                )
-            }
-          )
-        }
-        .getOrElse(fail("Not expected to be here"))
-    }
-
-    val sumDataSet =
-      sparkSession
-        .sql("SELECT AVG(nums._3 - nums._2 - nums._1) FROM nums")
-        .as[Double]
-        .debugConditionally()
-
-    val listOfDoubles = sumDataSet.collect().head
-    listOfDoubles shouldEqual (4.0 +- (0.00000001))
-  }
-
 }
