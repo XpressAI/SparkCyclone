f7d646fb30a73c1e8ac17a88d22d9a3a18c5a2bd
WGipfeli <w@gipfeli.sg>
2021-06-06 22:54:47 +0100

    Tidy up more

--- aurora4spark-parent/aurora4spark-sql-plugin/src/test/scala/com/nec/cmake/PairwiseAdditionSpec.scala
+++ aurora4spark-parent/aurora4spark-sql-plugin/src/test/scala/com/nec/cmake/PairwiseAdditionSpec.scala
@@ -89,34 +89,4 @@ final class PairwiseAdditionSpec
     assert(listOfDoubles == List(3, 5, 7, 9, 58))
   }
 
-  "We handle single column average with specific plan" in withSparkSession(
-    _.set("spark.sql.extensions", classOf[SparkSqlPlanExtension].getCanonicalName)
-      .set(COLUMN_VECTOR_OFFHEAP_ENABLED.key, "true")
-  ) { sparkSession =>
-    import sparkSession.implicits._
-
-    SparkSqlPlanExtension.rulesToApply.clear()
-
-    SparkSqlPlanExtension.rulesToApply.append { sparkPlan =>
-      SingleColumnAvgPlanExtractor
-        .matchPlan(sparkPlan)
-        .map(plan =>
-          ArrowAveragingPlan(plan.sparkPlan, CBased(CMakeBuilder.CLibPath.toString), plan.column)
-        )
-        .getOrElse(sys.error(s"Plan was not matched: ${sparkPlan}"))
-    }
-
-    val inputData: Seq[(Double, Double)] = Seq((10, 2), (20, 5), (30, 6), (40, 7))
-
-    val sumDataSet2 = inputData
-      .toDS()
-      .as[(Double, Double)]
-      .selectExpr("AVG(_1)")
-      .as[Double]
-      .debugConditionally()
-
-    val listOfDoubles = sumDataSet2.collect().toList
-    assert(listOfDoubles == List(25))
-  }
-
 }
