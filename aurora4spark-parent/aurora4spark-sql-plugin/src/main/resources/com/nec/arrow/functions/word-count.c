65c522fba8d60d00ccc5df337939fbbdc1396333
WGipfeli <w@gipfeli.sg>
2021-06-10 06:32:38 +0900

    Sum Avg subtract start code

--- aurora4spark-parent/aurora4spark-sql-plugin/src/main/resources/com/nec/arrow/functions/word-count.c
+++ aurora4spark-parent/aurora4spark-sql-plugin/src/main/resources/com/nec/arrow/functions/word-count.c
@@ -51,3 +51,26 @@ long count_strings(varchar_vector* strings, non_null_int_vector* counted_string_
     /** todo free the excess memory before returning **/
     return 0;
 }
+
+
+/** SUM(a-b), AVG(b) **/
+int sum_avg_subtract(non_null_double_vector* a, non_null_double_vector* b, non_null_double_vector* c, non_null_double_vector* the_sum, non_null_double_vector* the_avg) {
+    
+    the_sum->data = malloc(1 * sizeof(double));
+    the_avg->data = malloc(1 * sizeof(double));
+    the_sum->count = 1;
+    the_avg->count = 1;
+
+    double total_sum = 0;
+    double running_total = 0;
+    for ( int i = 0; i < a->count; i++ ) {
+         total_sum += a->data[i] - b->data[i];
+         running_total += b->data[i];
+    }
+
+    the_sum->data[0] = total_sum;
+    the_avg->data[0] = running_total / a->count;
+
+    return 0;
+}
+
