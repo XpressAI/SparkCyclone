61a79a0f4ae77222bacb0ab10b84f0fe0e80399e
Wosin <blind.van@gmail.com>
2021-06-12 15:31:06 +0200

    Test to verify wholestage codegen execution order.

--- aurora4spark-parent/aurora4spark-sql-plugin/src/test/scala/com/nec/spark/planning/SummingSparkPlanSpec.scala
+++ aurora4spark-parent/aurora4spark-sql-plugin/src/test/scala/com/nec/spark/planning/SummingSparkPlanSpec.scala
@@ -10,6 +10,7 @@ import org.scalatest.BeforeAndAfter
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
 
+import org.apache.spark.sql.execution.{SparkPlan, WholeStageCodegenExec}
 import org.apache.spark.sql.execution.aggregate.HashAggregateExec
 import org.apache.spark.sql.execution.exchange.ShuffleExchangeExec
 import org.apache.spark.sql.internal.SQLConf.WHOLESTAGE_CODEGEN_ENABLED
@@ -78,6 +79,53 @@ final class SummingSparkPlanSpec
 
     assert(executionPlan.head === seq.sum +- 0.0000000001)
   }
+
+  "Wholestage gen is executed after the plugin" in withSparkSession(
+    _.set(WHOLESTAGE_CODEGEN_ENABLED.key, "true")
+      .set("spark.sql.extensions", classOf[SparkSqlPlanExtension].getCanonicalName)
+
+  ) { sparkSession =>
+    import sparkSession.implicits._
+    val seq = Seq.fill(500)(Random.nextDouble())
+    seq.toDS()
+      .createOrReplaceTempView("nums")
+    SparkSqlPlanExtension.rulesToApply.clear()
+
+    var beforeMatchingPlan: SparkPlan = null
+
+    SparkSqlPlanExtension.rulesToApply.append {
+      sparkPlan => {
+        PartialSingleColumnSumPlanExtractor
+          .matchPlan(sparkPlan)
+          .map(plan => {
+
+            val newChild = PartialArrowSummingPlan(plan.child,
+              CBased(CMakeBuilder.CLibPath.toString),
+              plan.column)
+            val newMain = plan.replaceMain(newChild)
+            beforeMatchingPlan = newMain
+
+            newMain
+
+          })
+          .getOrElse(sys.error(s"Plan was not matched: ${sparkPlan}"))
+
+      }
+    }
+    val querySet = sparkSession
+      .sql("SELECT SUM(value) FROM nums")
+      .as[(Double)]
+
+    querySet.collect()
+    println(beforeMatchingPlan)
+    println(querySet.executionPlan)
+
+    assert(
+      beforeMatchingPlan.isInstanceOf[HashAggregateExec],
+      querySet.executionPlan.isInstanceOf[WholeStageCodegenExec]
+    )
+  }
+
 
   "Specific plan doesn't match sum of two columns" in withSparkSession(
     _.set(WHOLESTAGE_CODEGEN_ENABLED.key, "false")
