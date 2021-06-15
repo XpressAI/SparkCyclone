fb3bad6bf6dfce5c3a9cf4d8c1159d31c1f08c1f
Wosin <blind.van@gmail.com>
2021-06-16 01:14:21 +0200

    Test

--- aurora4spark-parent/aurora4spark-sql-plugin/src/main/resources/com/nec/arrow/functions/cpp/joiner.cc
+++ aurora4spark-parent/aurora4spark-sql-plugin/src/main/resources/com/nec/arrow/functions/cpp/joiner.cc
@@ -1,9 +1,10 @@
 #include "frovedis/core/radix_sort.hpp"
 #include "transfer-definitions.c"
 #include "frovedis/dataframe/join.hpp"
-
+#include <vector>
 extern "C" long join_doubles(non_null_double_vector* input, non_null_double_vector* out) {
     std::cout << "TEZT\n";
-    std::vector vector();
+    std::vector<int> vec;
+    vec.push_back(10);
    return 0;
 }
