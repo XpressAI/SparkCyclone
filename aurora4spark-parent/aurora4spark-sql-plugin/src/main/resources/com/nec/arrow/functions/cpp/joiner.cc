d3d3b8349ac88893ae1c10c6ada38f1e3633bb43
Wosin <blind.van@gmail.com>
2021-06-16 01:04:49 +0200

    Test.

--- aurora4spark-parent/aurora4spark-sql-plugin/src/main/resources/com/nec/arrow/functions/cpp/joiner.cc
+++ aurora4spark-parent/aurora4spark-sql-plugin/src/main/resources/com/nec/arrow/functions/cpp/joiner.cc
@@ -4,10 +4,12 @@
 extern "C" long join_doubles(non_null_double_vector* input, non_null_double_vector* out) {
     // ret.append_column(cnames[i], col);
 
-    std::vector<double> left_vec(input -> data, input->data + input -> count);
-    out->data = (double *)malloc(input->count * sizeof(double));
-    out->count = input->count;
-    memcpy(out->data, input -> data, input->count * sizeof(double));
-    frovedis::insertion_sort<double>(out->data, input->count);
-    return 0;
+    std::vector<double> left_vec(left -> data, left->data + left -> count);
+
+extern "C" long join_doubles(non_null_double_vector* left,
+    non_null_double_vector* right,
+    non_null_double_vector* out) {
+    std::cout << "TEZT\n";
+    frovedis::equi_join2(input, input, out);
+   return 0;
 }
