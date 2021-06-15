1dd4d48f5bd34a1a3743421223e9bb555afc1649
Wosin <blind.van@gmail.com>
2021-06-16 01:10:34 +0200

    Test.

--- aurora4spark-parent/aurora4spark-sql-plugin/src/main/resources/com/nec/arrow/functions/cpp/joiner.cc
+++ aurora4spark-parent/aurora4spark-sql-plugin/src/main/resources/com/nec/arrow/functions/cpp/joiner.cc
@@ -2,9 +2,7 @@
 #include "transfer-definitions.c"
 #include "frovedis/dataframe/join.hpp"
 
-extern "C" long join_doubles(non_null_double_vector* left,
-    non_null_double_vector* right,
-    non_null_double_vector* out) {
+extern "C" long join_doubles(non_null_double_vector* input, non_null_double_vector* out) {
     std::cout << "TEZT\n";
     frovedis::equi_join2(input, input, out);
    return 0;
