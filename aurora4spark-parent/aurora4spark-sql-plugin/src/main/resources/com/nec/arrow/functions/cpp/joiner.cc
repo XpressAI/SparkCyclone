ba631a92f150e477bd1240fe4d282ca99f05014b
Wosin <blind.van@gmail.com>
2021-06-16 01:05:52 +0200

    Test.

--- aurora4spark-parent/aurora4spark-sql-plugin/src/main/resources/com/nec/arrow/functions/cpp/joiner.cc
+++ aurora4spark-parent/aurora4spark-sql-plugin/src/main/resources/com/nec/arrow/functions/cpp/joiner.cc
@@ -1,10 +1,6 @@
 #include "frovedis/core/radix_sort.hpp"
 #include "transfer-definitions.c"
 #include "frovedis/dataframe/join.hpp"
-extern "C" long join_doubles(non_null_double_vector* input, non_null_double_vector* out) {
-    // ret.append_column(cnames[i], col);
-
-    std::vector<double> left_vec(left -> data, left->data + left -> count);
 
 extern "C" long join_doubles(non_null_double_vector* left,
     non_null_double_vector* right,
