b7e4473e605b703c360f6341d092cf69288cf22e
WGipfeli <w@gipfeli.sg>
2021-06-19 21:29:27 +0100

    Read in a columnar fashion for ArrowSummingPlan

--- aurora4spark-parent/aurora4spark-sql-plugin/src/main/scala/com/nec/spark/planning/ArrowSummingPlan.scala
+++ aurora4spark-parent/aurora4spark-sql-plugin/src/main/scala/com/nec/spark/planning/ArrowSummingPlan.scala
@@ -1,17 +1,20 @@
 package com.nec.spark.planning
 
 import com.nec.arrow.functions.Sum
-import com.nec.arrow.{CArrowNativeInterfaceNumeric, VeArrowNativeInterfaceNumeric}
+import com.nec.arrow.CArrowNativeInterfaceNumeric
+import com.nec.arrow.VeArrowNativeInterfaceNumeric
 import com.nec.spark.Aurora4SparkExecutorPlugin
 import com.nec.spark.agile.Column
 import com.nec.spark.planning.ArrowSummingPlan.ArrowSummer
-import org.apache.arrow.vector.{Float8Vector, VectorSchemaRoot}
-
+import org.apache.arrow.vector.Float8Vector
+import org.apache.arrow.vector.VectorSchemaRoot
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.catalyst.InternalRow
-import org.apache.spark.sql.catalyst.expressions.{Attribute, AttributeReference}
+import org.apache.spark.sql.catalyst.expressions.Attribute
+import org.apache.spark.sql.catalyst.expressions.AttributeReference
 import org.apache.spark.sql.execution.SparkPlan
 import org.apache.spark.sql.execution.arrow.ArrowWriter
+import org.apache.spark.sql.execution.arrow.ColumnarArrowWriter
 import org.apache.spark.sql.execution.vectorized.OffHeapColumnVector
 import org.apache.spark.sql.types.DoubleType
 import org.apache.spark.sql.util.ArrowUtilsExposed
@@ -55,31 +58,60 @@ case class ArrowSummingPlan(child: SparkPlan, summer: ArrowSummer, column: Colum
   override def supportsColumnar: Boolean = true
 
   override protected def doExecuteColumnar(): RDD[ColumnarBatch] = {
-    child
-      .execute()
-      .mapPartitions { rows =>
-        val timeZoneId = conf.sessionLocalTimeZone
-        val allocator = ArrowUtilsExposed.rootAllocator.newChildAllocator(
-          s"writer for word count",
-          0,
-          Long.MaxValue
-        )
-        val arrowSchema = ArrowUtilsExposed.toArrowSchema(schema, timeZoneId)
-        val root = VectorSchemaRoot.create(arrowSchema, allocator)
-        val arrowWriter = ArrowWriter.create(root)
-        rows.foreach(row => arrowWriter.write(row))
-        arrowWriter.finish()
+    println(s"Supports columnar? ${child.supportsColumnar}")
+    if (child.supportsColumnar) {
+      child
+        .executeColumnar()
+        .map { colBatch =>
+          val timeZoneId = conf.sessionLocalTimeZone
+          val allocator = ArrowUtilsExposed.rootAllocator.newChildAllocator(
+            s"writer for word count",
+            0,
+            Long.MaxValue
+          )
+          val arrowSchema = ArrowUtilsExposed.toArrowSchema(schema, timeZoneId)
+          val root = VectorSchemaRoot.create(arrowSchema, allocator)
+          val arrowWriter = ColumnarArrowWriter.create(root)
+          arrowWriter.writeColumn(colBatch)
+          arrowWriter.finish()
 
-        Iterator(summer.sum(root.getVector(0).asInstanceOf[Float8Vector], 1))
-      }
-      .coalesce(1)
-      .mapPartitions { it =>
-        val result = it.reduce((a, b) => a + b)
-        val outVector = new OffHeapColumnVector(1, DoubleType)
-        outVector.putDouble(0, result)
+          summer.sum(root.getVector(0).asInstanceOf[Float8Vector], 1)
+        }
+        .coalesce(1)
+        .mapPartitions { it =>
+          val result = it.reduce((a, b) => a + b)
+          val outVector = new OffHeapColumnVector(1, DoubleType)
+          outVector.putDouble(0, result)
 
-        Iterator(new ColumnarBatch(Array(outVector), 1))
-      }
+          Iterator(new ColumnarBatch(Array(outVector), 1))
+        }
+    } else {
+      child
+        .execute()
+        .mapPartitions { rows =>
+          val timeZoneId = conf.sessionLocalTimeZone
+          val allocator = ArrowUtilsExposed.rootAllocator.newChildAllocator(
+            s"writer for word count",
+            0,
+            Long.MaxValue
+          )
+          val arrowSchema = ArrowUtilsExposed.toArrowSchema(schema, timeZoneId)
+          val root = VectorSchemaRoot.create(arrowSchema, allocator)
+          val arrowWriter = ArrowWriter.create(root)
+          rows.foreach(row => arrowWriter.write(row))
+          arrowWriter.finish()
+
+          Iterator(summer.sum(root.getVector(0).asInstanceOf[Float8Vector], 1))
+        }
+        .coalesce(1)
+        .mapPartitions { it =>
+          val result = it.reduce((a, b) => a + b)
+          val outVector = new OffHeapColumnVector(1, DoubleType)
+          outVector.putDouble(0, result)
+
+          Iterator(new ColumnarBatch(Array(outVector), 1))
+        }
+    }
   }
 
   override def output: Seq[Attribute] = Seq(
