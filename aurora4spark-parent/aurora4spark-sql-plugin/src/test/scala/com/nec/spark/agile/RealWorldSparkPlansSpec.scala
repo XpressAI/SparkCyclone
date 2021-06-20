8942b2bcff2d7bcdc09dd77910f7eca5e5dc5516
WGipfeli <w@gipfeli.sg>
2021-06-20 14:35:38 +0100

    Import VEHashAggregate, which is based on HashAggregateExec

--- aurora4spark-parent/aurora4spark-sql-plugin/src/test/scala/com/nec/spark/agile/RealWorldSparkPlansSpec.scala
+++ aurora4spark-parent/aurora4spark-sql-plugin/src/test/scala/com/nec/spark/agile/RealWorldSparkPlansSpec.scala
@@ -6,6 +6,13 @@ import java.util.Properties
 import com.nec.h2.H2DatabaseConnector
 import com.nec.spark.SampleTestData
 import com.nec.spark.SparkAdditions
+import org.apache.spark.sql.catalyst.expressions.aggregate.AggregateExpression
+import org.apache.spark.sql.catalyst.expressions.aggregate.Sum
+import org.apache.spark.sql.catalyst.rules.Rule
+import org.apache.spark.sql.execution.ColumnarRule
+import org.apache.spark.sql.execution.SparkPlan
+import org.apache.spark.sql.execution.aggregate.HashAggregateExec
+import org.apache.spark.sql.execution.aggregate.VEHashAggregate
 import org.scalatest.BeforeAndAfter
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
@@ -40,5 +47,88 @@ final class RealWorldSparkPlansSpec
           "GROUP BY userId, joined_timestamp ORDER BY joined_timestamp ASC"
       )
       .debugSqlAndShow(name = "real-life-example")
+  }
+  "We get the spark plan for a simple sum" in withSparkSession(identity) { sparkSession =>
+    import sparkSession.implicits._
+
+    sparkSession.read
+      .option("header", "true")
+      .csv(SampleTestData.OrdersCsv.toString)
+      .createOrReplaceTempView("Orders")
+
+    val result = sparkSession
+      .sql(sqlText = "SELECT SUM(totalPrice) FROM Orders")
+      .debugSqlAndShow(name = "sum-total-order-price")
+      .as[Double]
+      .collect()
+      .head
+
+    assert(result == 16305.0)
+  }
+  "We get the spark plan for a simple sum rewritten" in withSparkSession2(
+    _.withExtensions(extensions =>
+      extensions.injectColumnar({ sparkSession =>
+        new ColumnarRule {
+          override def preColumnarTransitions: Rule[SparkPlan] = sparkPlan =>
+            PartialFunction
+              .condOpt(sparkPlan) {
+                case first @ HashAggregateExec(
+                      requiredChildDistributionExpressions,
+                      groupingExpressions,
+                      Seq(AggregateExpression(avg @ Sum(exr), mode, isDistinct, filter, resultId)),
+                      aggregateAttributes,
+                      initialInputBufferOffset,
+                      resultExpressions,
+                      cc @ org.apache.spark.sql.execution.exchange
+                        .ShuffleExchangeExec(
+                          outputPartitioning,
+                          org.apache.spark.sql.execution.aggregate
+                            .HashAggregateExec(
+                              _requiredChildDistributionExpressions,
+                              _groupingExpressions,
+                              _aggregateExpressions,
+                              _aggregateAttributes,
+                              _initialInputBufferOffset,
+                              _resultExpressions,
+                              fourth
+                            ),
+                          shuffleOrigin
+                        )
+                    ) if (avg.references.size == 1) =>
+                  println("MATCHED!!!!")
+                  first.copy(child =
+                    cc.copy(child =
+                      VEHashAggregate(
+                        _requiredChildDistributionExpressions,
+                        _groupingExpressions,
+                        _aggregateExpressions,
+                        _aggregateAttributes,
+                        _initialInputBufferOffset,
+                        _resultExpressions,
+                        fourth
+                      )
+                    )
+                  )
+              }
+              .getOrElse(sparkPlan)
+        }
+      })
+    )
+  ) { sparkSession =>
+    import sparkSession.implicits._
+
+    sparkSession.read
+      .option("header", "true")
+      .csv(SampleTestData.OrdersCsv.toString)
+      .createOrReplaceTempView("Orders")
+
+    val result = sparkSession
+      .sql(sqlText = "SELECT SUM(totalPrice) FROM Orders")
+      .debugSqlAndShow(name = "sum-total-order-price-2")
+      .as[Double]
+      .collect()
+      .head
+
+    assert(result == 16305.0)
   }
 }
