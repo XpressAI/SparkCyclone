f1eec63bfce981a7512f7b75b9baec7cd37582cf
WGipfeli <w@gipfeli.sg>
2021-06-26 14:34:59 +0100

    Do a logical summing plan, rather than a physical one, it looks much simpler

---
+++ aurora4spark-parent/aurora4spark-sql-plugin/src/test/scala/com/nec/spark/agile/LogicalSummingPlanSpec.scala
@@ -0,0 +1,117 @@
+package com.nec.spark.agile
+
+import com.nec.spark.SampleTestData.SampleCSV
+import com.nec.spark.SampleTestData.SampleTwoColumnParquet
+import com.nec.spark.SparkAdditions
+import com.nec.spark.agile.LogicalSummingPlanSpec.SomePlan
+import com.nec.spark.agile.LogicalSummingPlanSpec.makeCsvNums
+import com.nec.spark.agile.LogicalSummingPlanSpec.makeMemoryNums
+import com.nec.spark.agile.LogicalSummingPlanSpec.makeParquetNums
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.Encoder
+import org.apache.spark.sql.Encoders
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.Strategy
+import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.catalyst.expressions.Attribute
+import org.apache.spark.sql.catalyst.plans.logical
+import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
+import org.apache.spark.sql.execution.SparkPlan
+import org.apache.spark.sql.internal.SQLConf.CODEGEN_FALLBACK
+import org.apache.spark.sql.types.DoubleType
+import org.apache.spark.sql.types.StructField
+import org.apache.spark.sql.types.StructType
+import org.scalatest.BeforeAndAfter
+import org.scalatest.freespec.AnyFreeSpec
+
+object LogicalSummingPlanSpec {
+  case class SomePlan(child: SparkPlan) extends SparkPlan {
+    override protected def doExecute(): RDD[InternalRow] = child.execute()
+    override def output: Seq[Attribute] = child.output
+    override def children: Seq[SparkPlan] = Seq(child)
+  }
+
+  val SharedName = "nums"
+
+  def makeMemoryNums(sparkSession: SparkSession): Unit = {
+    import sparkSession.implicits._
+    Seq(1d, 2d, 3d, 4d, 52d)
+      .toDS()
+      .createOrReplaceTempView(SharedName)
+  }
+
+  def makeCsvNums(sparkSession: SparkSession): Unit = {
+    import sparkSession.implicits._
+    val schema = StructType(Array(StructField("a", DoubleType)))
+    sparkSession.read
+      .format("csv")
+      .schema(schema)
+      .load(SampleCSV.toString)
+      .withColumnRenamed("a", "value")
+      .as[Double]
+      .createOrReplaceTempView(SharedName)
+  }
+
+  def makeParquetNums(sparkSession: SparkSession): Unit = {
+    import sparkSession.implicits._
+    sparkSession.read
+      .format("parquet")
+      .load(SampleTwoColumnParquet.toString)
+      .withColumnRenamed("a", "value")
+      .as[(Double, Double)]
+      .createOrReplaceTempView(SharedName)
+  }
+
+}
+
+//noinspection ConvertExpressionToSAM
+final class LogicalSummingPlanSpec extends AnyFreeSpec with BeforeAndAfter with SparkAdditions {
+
+  private implicit val encDouble: Encoder[Double] = Encoders.scalaDouble
+
+  "We can do a sum" - {
+    withVariousInputs[Double](
+      _.config(CODEGEN_FALLBACK.key, value = false)
+        .config("spark.sql.codegen.comments", value = true)
+        .withExtensions(sse =>
+          sse.injectPlannerStrategy(sparkSession =>
+            new Strategy {
+              override def apply(plan: LogicalPlan): Seq[SparkPlan] =
+                plan match {
+                  case logical.Aggregate(groupingExpressions, resultExpressions, child) =>
+                    List(SomePlan(planLater(child)))
+                  case _ => Nil
+                }
+            }
+          )
+        )
+    )("SELECT SUM(value) FROM nums")(result => assert(result == List(6d)))
+  }
+
+  implicit class RichDataSet[T](val dataSet: Dataset[T]) {
+    def debugSqlHere: Dataset[T] = {
+      info(dataSet.queryExecution.executedPlan.toString())
+//      info (   dataSet.queryExecution.debug.codegenToSeq().mkString("\n"))
+      dataSet
+    }
+  }
+
+  def withVariousInputs[T](
+    configuration: SparkSession.Builder => SparkSession.Builder
+  )(sql: String)(f: List[T] => Unit)(implicit enc: Encoder[T]): Unit = {
+    for {
+      (title, fr) <- List(
+        "Memory" -> makeMemoryNums _,
+        "CSV" -> makeCsvNums _,
+        "Parquet" -> makeParquetNums _
+      )
+    } s"In ${title}" in withSparkSession2(configuration) { sparkSession =>
+      import sparkSession.implicits._
+      fr(sparkSession)
+      val ds = sparkSession.sql(sql).debugSqlHere.as[T]
+      f(ds.collect().toList)
+    }
+  }
+
+}
