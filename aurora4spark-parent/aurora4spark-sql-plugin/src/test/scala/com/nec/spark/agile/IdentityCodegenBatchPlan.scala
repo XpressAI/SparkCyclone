7bf7ac256a7021a3d216013f03f6c2db1df14a47
WGipfeli <w@gipfeli.sg>
2021-06-26 23:26:27 +0100

    Now we got a working base, that consumes Parquets, which is great!

--- aurora4spark-parent/aurora4spark-sql-plugin/src/test/scala/com/nec/spark/agile/IdentityCodegenBatchPlan.scala
+++ aurora4spark-parent/aurora4spark-sql-plugin/src/test/scala/com/nec/spark/agile/IdentityCodegenBatchPlan.scala
@@ -4,9 +4,6 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.expressions.Attribute
 import org.apache.spark.sql.catalyst.expressions.SortOrder
-import org.apache.spark.sql.catalyst.expressions.codegen.CodeGenerator
-import org.apache.spark.sql.catalyst.expressions.codegen.CodegenContext
-import org.apache.spark.sql.catalyst.expressions.codegen.ExprCode
 import org.apache.spark.sql.catalyst.plans.physical.Partitioning
 import org.apache.spark.sql.execution.BlockingOperatorWithCodegen
 import org.apache.spark.sql.execution.CodegenSupport
@@ -14,74 +11,15 @@ import org.apache.spark.sql.execution.SparkPlan
 
 final case class IdentityCodegenBatchPlan(child: SparkPlan)
   extends SparkPlan
-  with BlockingOperatorWithCodegen {
-  type ContainerType = UnsafeExternalDuplicator
-  def createContainer(): UnsafeExternalDuplicator = new UnsafeExternalDuplicator
+  with BlockingOperatorWithCodegen
+  with UnsafeExternalProcessorBase {
+  override type ContainerType = UnsafeExternalDuplicator
+  override def containerClass: Class[ContainerType] = classOf[UnsafeExternalDuplicator]
+  override def createContainer(): UnsafeExternalDuplicator = new UnsafeExternalDuplicator
   override def children: Seq[SparkPlan] = Seq(child)
-
   protected def inputAttributes: Seq[Attribute] = child.output
-
-  override protected def doExecute(): RDD[InternalRow] =
-    sys.error("This should not be called if in WSCG")
-
   override def output: Seq[Attribute] = child.output
-
   override def inputRDDs(): Seq[RDD[InternalRow]] = child.asInstanceOf[CodegenSupport].inputRDDs()
-
   override def outputOrdering: Seq[SortOrder] = child.outputOrdering
-
   override def outputPartitioning: Partitioning = child.outputPartitioning
-
-  override protected def doProduce(ctx: CodegenContext): String = {
-    val executed = ctx.addMutableState(CodeGenerator.JAVA_BOOLEAN, "executed")
-    val outputRow = ctx.freshName("outputRow")
-    val thisPlan = ctx.addReferenceObj("plan", this)
-    containerVariable = ctx.addMutableState(
-      classOf[UnsafeExternalDuplicator].getName,
-      "batchProcessor",
-      v => s"$v = $thisPlan.createContainer();",
-      forceInline = true
-    )
-    ctx.INPUT_ROW = null
-    val resultIterator = ctx.addMutableState(
-      "scala.collection.Iterator<UnsafeRow>",
-      "batchResultsIterator",
-      forceInline = true
-    )
-
-    val doAgg = ctx.freshName("doAggregateWithoutKey")
-    val doAggFuncName = ctx.addNewFunction(
-      doAgg,
-      s"""
-         |private void $doAgg() throws java.io.IOException {
-         |  // initialize aggregation buffer
-         |  System.out.println("Doing a produce here in Batch Planner...");
-         |  ${child.asInstanceOf[CodegenSupport].produce(ctx, this)}
-         |}
-       """.stripMargin
-    )
-
-    s"""
-    if(!$executed) {
-      $executed = true;
-      $doAggFuncName();
-      $resultIterator = $containerVariable.execute();
-    }
-    while ($limitNotReachedCond $resultIterator.hasNext()) {
-      UnsafeRow $outputRow = (UnsafeRow)$resultIterator.next();
-      ${consume(ctx, null, outputRow)}
-      if (shouldStop()) return;
-    }
-     """
-  }
-
-  @transient private var containerVariable: String = _
-
-  override def doConsume(ctx: CodegenContext, input: Seq[ExprCode], row: ExprCode): String = {
-    s"""
-       ${row.code}
-       $containerVariable.insertRow((UnsafeRow)${row.value});
-     """.stripMargin
-  }
-
 }
--- aurora4spark-parent/aurora4spark-sql-plugin/src/test/scala/com/nec/spark/agile/UnsafeExternalProcessorBase.scala
+++ aurora4spark-parent/aurora4spark-sql-plugin/src/test/scala/com/nec/spark/agile/UnsafeExternalProcessorBase.scala
@@ -10,24 +10,18 @@ import org.apache.spark.sql.execution.CodegenSupport
 import org.apache.spark.sql.execution.SparkPlan
 
 trait UnsafeExternalProcessorBase { this: SparkPlan with BlockingOperatorWithCodegen =>
-
   def child: SparkPlan
-
   override protected def doExecute(): RDD[InternalRow] = sys.error("This should not be called if in WSCG")
   override def inputRDDs(): Seq[RDD[InternalRow]] = Seq(child.execute())
-
   type ContainerType <: UnsafeBatchProcessor
-
   def containerClass: Class[ContainerType]
-
   def createContainer(): ContainerType
-
   override protected def doProduce(ctx: CodegenContext): String = {
     val executed = ctx.addMutableState(CodeGenerator.JAVA_BOOLEAN, "executed")
     val outputRow = ctx.freshName("outputRow")
     val thisPlan = ctx.addReferenceObj("plan", this)
     containerVariable = ctx.addMutableState(
-      containerClass.getName,
+      classOf[UnsafeExternalDuplicator].getName,
       "batchProcessor",
       v => s"$v = $thisPlan.createContainer();",
       forceInline = true
@@ -35,33 +29,43 @@ trait UnsafeExternalProcessorBase { this: SparkPlan with BlockingOperatorWithCod
     ctx.INPUT_ROW = null
     val resultIterator = ctx.addMutableState(
       "scala.collection.Iterator<UnsafeRow>",
-      "resultsIterator",
+      "batchResultsIterator",
       forceInline = true
     )
 
+    val doConsumeMethodIdx = ctx.freshName("doConsumeChild")
+    val doConsumeFunctionName = ctx.addNewFunction(
+      doConsumeMethodIdx,
+      s"""
+         |private void $doConsumeMethodIdx() throws java.io.IOException {
+         |  // initialize aggregation buffer
+         |  System.out.println("Doing a produce here in Batch Planner...");
+         |  ${child.asInstanceOf[CodegenSupport].produce(ctx, this)}
+         |}
+       """.stripMargin
+    )
     s"""
-
     if(!$executed) {
       $executed = true;
-      ${child.asInstanceOf[CodegenSupport].produce(ctx, this)}
+      $doConsumeFunctionName();
       $resultIterator = $containerVariable.execute();
     }
-
-//    while ($limitNotReachedCond $resultIterator.hasNext()) {
-//      UnsafeRow $outputRow = (UnsafeRow)$resultIterator.next();
-//      ${consume(ctx, null, outputRow)}
-//      if (shouldStop()) return;
-//    }
-   """
+    while ($limitNotReachedCond $resultIterator.hasNext()) {
+      UnsafeRow $outputRow = (UnsafeRow)$resultIterator.next();
+      ${consume(ctx, null, outputRow)}
+      if (shouldStop()) return;
+    }
+     """
   }
 
-  private var containerVariable: String = _
+  @transient private var containerVariable: String = _
 
   override def doConsume(ctx: CodegenContext, input: Seq[ExprCode], row: ExprCode): String = {
     s"""
-       |${row.code}
-//       |$containerVariable.insertRow((UnsafeRow)${row.value});
+       ${row.code}
+       $containerVariable.insertRow((UnsafeRow)${row.value});
      """.stripMargin
   }
+
 
 }
