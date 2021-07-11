3b1f85c9806a6bca5dff8dbfbd25788adcc16f7e
Wosin <blind.van@gmail.com>
2021-07-11 20:07:41 +0200

    D group plan (#129)
    * Implement additional function handling GroupBy with sum.

--- src/main/resources/com/nec/arrow/functions/cpp/grouper.cc
+++ src/main/resources/com/nec/arrow/functions/cpp/grouper.cc
@@ -50,3 +50,45 @@ extern "C" long group_by(non_null_double_vector* grouping_col,
 
      return 0;
 }
+
+
+extern "C" long group_by_sum(non_null_double_vector* grouping_col,
+                         non_null_double_vector* values_col,
+                         non_null_double_vector* values,
+                         non_null_double_vector* groups
+                         ) {
+
+     std::vector<double> grouping_vec(grouping_col -> data, grouping_col->data + grouping_col -> count);
+     std::vector<size_t> idx;
+     for(size_t i = 0; i < grouping_col->count; i++) {
+       idx.push_back(i);
+     }
+     frovedis::radix_sort(grouping_vec, idx, grouping_vec.size());
+
+     values->data = (double *) malloc(grouping_vec.size() * sizeof(double));
+     values->count = grouping_vec.size();
+     //Pre-allocate enough data so that we can return even if only unique ids are there
+     groups->data = (double *) malloc(values_col->count * sizeof(double));
+
+     double last = grouping_vec[0];
+     double curr_sum = 0.0;
+     int groups_count = 0;
+     for(size_t i = 0; i < grouping_vec.size(); i++) {
+        if(grouping_vec[i] == last) {
+          curr_sum += values_col->data[idx[i]];
+        } else {
+          groups->data[groups_count] = last;
+          values->data[groups_count] = curr_sum;
+          last = grouping_vec[i];
+          groups_count++;
+          curr_sum = values_col->data[idx[i]];
+        }
+     }
+     groups->data[groups_count] = last;
+     values->data[groups_count] = curr_sum;
+     groups_count++;
+     groups->count = groups_count;
+     values->count = groups_count;
+
+     return 0;
+}
---
+++ src/main/scala/com/nec/arrow/functions/GroupBySum.scala
@@ -0,0 +1,49 @@
+package com.nec.arrow.functions
+
+import com.nec.arrow.ArrowNativeInterfaceNumeric
+import com.nec.arrow.ArrowNativeInterfaceNumeric.SupportedVectorWrapper.{Float8VectorWrapper, IntVectorWrapper}
+import org.apache.arrow.vector.{Float8Vector, IntVector}
+
+object GroupBySum {
+
+  val GroupBySumSourceCode: String = {
+    val source = scala.io.Source.fromInputStream(getClass.getResourceAsStream("/com/nec/arrow/functions/cpp/grouper.cc"))
+    try source.mkString
+    finally source.close()
+  }
+
+  def runOn(nativeInterface: ArrowNativeInterfaceNumeric)(
+    groupingVector: Float8Vector,
+    valuesVector: Float8Vector,
+    outputGroupsVector: Float8Vector,
+    outputValuesVector: Float8Vector
+  ): Unit = {
+
+    nativeInterface.callFunction(
+      name = "group_by_sum",
+      inputArguments = List(
+        Some(Float8VectorWrapper(groupingVector)),
+        Some(Float8VectorWrapper(valuesVector)),
+        None,
+        None,
+      ),
+      outputArguments = List(
+        None,
+        None,
+        Some(Float8VectorWrapper(outputValuesVector)),
+        Some(Float8VectorWrapper(outputGroupsVector)),
+      )
+    )
+  }
+
+  def groupBySumJVM(groupingVector: Float8Vector, valuesVector: Float8Vector): Map[Double, Double] = {
+    val groupingVals = (0 until groupingVector.getValueCount).map(idx => groupingVector.get(idx))
+    val valuesVals = (0 until valuesVector.getValueCount).map(idx => valuesVector.get(idx))
+
+    groupingVals
+      .zip(valuesVals)
+      .groupBy(_._1)
+      .map(kv => (kv._1, kv._2.map(_._2).sum))
+
+  }
+}
