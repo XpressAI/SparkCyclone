88e6691cc3d6c13dcbe884ad027c7830fcfa7e32
WGipfeli <w@gipfeli.sg>
2021-07-12 05:45:48 +0900

    Log better

--- src/test/scala/com/nec/ve/DynamicBenchmarkVeCheck.scala
+++ src/test/scala/com/nec/ve/DynamicBenchmarkVeCheck.scala
@@ -5,11 +5,20 @@ import org.scalatest.freespec.AnyFreeSpec
 import com.nec.spark.Aurora4SparkExecutorPlugin
 import com.nec.spark.BenchTestingPossibilities.BenchTestAdditions
 import org.scalatest.BeforeAndAfterAll
+import org.scalatest.ConfigMap
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
 
 final class DynamicBenchmarkVeCheck
   extends AnyFreeSpec
   with BeforeAndAfterAll
   with BenchTestAdditions {
+
+  override protected def beforeAll(): Unit = {
+    val rootLogger = Logger.getRootLogger
+    rootLogger.setLevel(Level.INFO)
+    super.beforeAll()
+  }
 
   /** TODO We could also generate Spark plan details from here for easy cross-referencing, as well as codegen */
   BenchTestingPossibilities.possibilities.filter(_.testingTarget.isVE).foreach(runTestCase)
