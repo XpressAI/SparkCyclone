48c89b5c7b40fa0d6bcbb6fef941ac2d45c22efe
WGipfeli <w@gipfeli.sg>
2021-07-11 20:52:17 +0100

    For test files

--- src/main/scala/com/nec/cmake/CNativeEvaluator.scala
+++ src/main/scala/com/nec/cmake/CNativeEvaluator.scala
@@ -1,8 +1,8 @@
 package com.nec.cmake
 import com.nec.arrow.CArrowNativeInterfaceNumeric
 import com.nec.arrow.ArrowNativeInterfaceNumeric
-import com.nec.spark.planning.CEvaluationPlan.NativeEvaluator
 import com.nec.arrow.TransferDefinitions
+import com.nec.spark.planning.NativeEvaluator
 
 object CNativeEvaluator extends NativeEvaluator {
   override def forCode(code: String): ArrowNativeInterfaceNumeric = {
--- src/main/scala/com/nec/spark/planning/VERewriteStrategy.scala
+++ src/main/scala/com/nec/spark/planning/VERewriteStrategy.scala
@@ -1,4 +1,5 @@
 package com.nec.spark.planning
+
 import org.apache.spark.sql.catalyst.plans.logical
 import org.apache.spark.sql.catalyst.expressions.aggregate.AggregateExpression
 import com.nec.spark.agile.CExpressionEvaluation.NameCleaner
@@ -6,7 +7,6 @@ import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
 import org.apache.spark.sql.execution.SparkPlan
 import com.nec.spark.agile.CExpressionEvaluation
 import com.nec.spark.agile.CExpressionEvaluation.RichListStr
-import com.nec.spark.planning.CEvaluationPlan.NativeEvaluator
 import com.nec.spark.planning.VERewriteStrategy.meldAggregateAndProject
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.Strategy
