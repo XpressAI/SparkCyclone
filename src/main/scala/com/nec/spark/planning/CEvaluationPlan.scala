9881508fe5126954c5a13e5ed07cd90965779db7
WGipfeli <w@gipfeli.sg>
2021-07-15 20:28:29 +0100

    Ok better

--- src/main/scala/com/nec/spark/planning/CEvaluationPlan.scala
+++ src/main/scala/com/nec/spark/planning/CEvaluationPlan.scala
@@ -202,6 +202,7 @@ final case class CEvaluationPlan(
 
         val uuid = java.util.UUID.randomUUID()
         logger.debug(s"[$uuid] loading input vectors...")
+
         val inputVectors: List[Float8Vector] = inputAttributes.zipWithIndex.map {
           case (attr, idx) =>
             columnarBatch.column(idx) match {
