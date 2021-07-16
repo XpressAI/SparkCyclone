1000246d88bc1d9dca32bfabce42a188731c3523
William <52247468+wgipfeli@users.noreply.github.com>
2021-07-16 01:44:49 +0100

    Use ByteArray instead of String when reading CSV (#136)
    * Skip Stringification step as it's redundant.
    
    * Skip Stringification step as it's redundant.
    Put behind a configuration flag.
    
    * Fix Wrapper
    
    * Skip by default!

--- src/main/scala/com/nec/spark/planning/NativeCsvExec.scala
+++ src/main/scala/com/nec/spark/planning/NativeCsvExec.scala
@@ -1,17 +1,17 @@
 package com.nec.spark.planning
+import com.nec.arrow.ArrowNativeInterfaceNumeric.SupportedVectorWrapper._
 import org.apache.spark.sql.catalyst.expressions.Attribute
 import org.apache.spark.sql.vectorized.ColumnarBatch
 import org.apache.spark.rdd.RDD
-import com.nec.arrow.ArrowNativeInterfaceNumeric.SupportedVectorWrapper.Float8VectorWrapper
-import com.nec.arrow.ArrowNativeInterfaceNumeric.SupportedVectorWrapper.StringWrapper
 import org.apache.spark.sql.execution.datasources.InMemoryFileIndex
 import org.apache.arrow.vector.Float8Vector
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.util.ArrowUtilsExposed
 import org.apache.spark.sql.execution.LeafExecNode
 import com.nec.arrow.functions.CsvParse
+import com.nec.spark.planning.NativeCsvExec.SkipStringsKey
 import com.typesafe.scalalogging.LazyLogging
-import org.apache.spark.internal.Logging
+import org.apache.spark.WholeTextFileRawRDD.RichSparkContext
 import org.apache.spark.sql.execution.SparkPlan
 import org.apache.spark.sql.Strategy
 import org.apache.spark.sql.catalyst.planning.ScanOperation
@@ -20,6 +20,8 @@ import org.apache.spark.sql.execution.datasources.HadoopFsRelation
 import org.apache.spark.sql.execution.datasources.LogicalRelation
 import org.apache.spark.sql.execution.datasources.csv.CSVFileFormat
 import org.apache.spark.sql.vectorized.ArrowColumnVector
+
+import java.nio.ByteBuffer
 
 object NativeCsvExec {
   case class NativeCsvStrategy(nativeEvaluator: NativeEvaluator) extends Strategy {
@@ -45,6 +47,8 @@ object NativeCsvExec {
         .toList
     }
   }
+
+  val SkipStringsKey = "spark.com.nec.native-csv-skip-strings"
 }
 
 case class NativeCsvExec(
@@ -59,6 +63,42 @@ case class NativeCsvExec(
     "Source here is only columnar"
   )
   override protected def doExecuteColumnar(): RDD[ColumnarBatch] = {
+    if (sparkContext.getConf.getBoolean(key = SkipStringsKey, defaultValue = true))
+      doExecuteColumnarByteArray()
+    else
+      doExecuteColumnarString()
+  }
+
+  protected def doExecuteColumnarByteArray(): RDD[ColumnarBatch] = {
+    val numColumns = output.size
+    val evaluator = nativeEvaluator.forCode(CsvParse.CsvParseCode)
+    val imfi = hadoopRelation.location.asInstanceOf[InMemoryFileIndex]
+    sparkContext.wholeRawTextFiles(imfi.rootPaths.head.toString).map { case (name, text) =>
+      val allocator = ArrowUtilsExposed.rootAllocator
+        .newChildAllocator(s"CSV read allocator", 0, Long.MaxValue)
+
+      val outColumns = (0 until numColumns).map { idx =>
+        new Float8Vector(s"out_${idx}", allocator)
+      }.toList
+      val startTime = System.currentTimeMillis()
+
+      evaluator.callFunction(
+        name = if (numColumns == 3) "parse_csv" else s"parse_csv_${numColumns}",
+        inputArguments = List(
+          Some(ByteBufferWrapper(ByteBuffer.wrap(text.getBytes), text.getBytes.length))
+        ) ++ outColumns.map(_ => None),
+        outputArguments = List(None) ++ outColumns.map(col => Some(Float8VectorWrapper(col)))
+      )
+      val millis = System.currentTimeMillis() - startTime
+      logInfo(s"Took ${millis} ms to process CSV: ${name} (${text.getLength} bytes)")
+      new ColumnarBatch(
+        outColumns.map(col => new ArrowColumnVector(col)).toArray,
+        outColumns.head.getValueCount
+      )
+    }
+  }
+
+  protected def doExecuteColumnarString(): RDD[ColumnarBatch] = {
     val numColumns = output.size
     val evaluator = nativeEvaluator.forCode(CsvParse.CsvParseCode)
     val imfi = hadoopRelation.location.asInstanceOf[InMemoryFileIndex]
@@ -84,4 +124,5 @@ case class NativeCsvExec(
       )
     }
   }
+
 }
