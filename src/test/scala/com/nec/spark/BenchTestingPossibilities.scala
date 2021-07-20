dc14e3bd1902daeb863a831d53025d1bd6cb0ee5
Wosin <blind.van@gmail.com>
2021-07-21 01:07:11 +0200

    Add COUNT to benchmarks.

--- src/test/scala/com/nec/spark/BenchTestingPossibilities.scala
+++ src/test/scala/com/nec/spark/BenchTestingPossibilities.scala
@@ -72,14 +72,14 @@ object BenchTestingPossibilities {
   import com.eed3si9n.expecty.Expecty.assert
   final case class SimpleSql(
     sql: String,
-    expectedResult: (Double, Double),
+    expectedResult: (Double, Double, Long),
     source: SampleSource,
     testingTarget: TestingTarget,
     offHeapMode: Option[VeColumnMode],
     csvStrategy: Option[CsvStrategy]
   ) extends Testing {
 
-    type Result = (Double, Double)
+    type Result = (Double, Double, Long)
 
     override def verifyResult(result: List[Result]): Unit = {
       assert(result == List(expectedResult))
@@ -213,7 +213,7 @@ object BenchTestingPossibilities {
             VeColumnMode.All.map(v => Some(v))
           else List(None)
       } yield SimpleSql(
-        sql = s"SELECT SUM(${SampleColA}), AVG(${SampleColB}) FROM nums",
+        sql = s"SELECT SUM(${SampleColA}), AVG(${SampleColB}), COUNT(*) FROM nums",
         expectedResult = (62, 4),
         source = source,
         testingTarget = testingTarget,
