816c94dd06176e712353bd082d614be4278d5fd7
Eduardo Gonzalez <me@wm-eddie.com>
2021-07-25 17:50:19 +0900

    Switch simple test to parquet

--- tests/simple/groups_ve.sh
+++ tests/simple/groups_ve.sh
@@ -8,15 +8,17 @@ $SPARK_HOME/bin/spark-submit \
     --conf spark.com.nec.spark.ncc.path=/opt/nec/ve/bin/ncc \
     --jars /opt/aurora4spark/aurora4spark-sql-plugin.jar \
     --conf spark.executor.extraClassPath=/opt/aurora4spark/aurora4spark-sql-plugin.jar \
-    --conf spark.executor.extraJavaOptions=-agentpath:/opt/yjp/bin/linux-x86-64/libyjpagent.so \
     --conf spark.plugins=com.nec.spark.AuroraSqlPlugin \
     --conf spark.sql.columnVector.offheap.enabled=true \
-    --conf spark.com.nec.native-csv=VE \
+    --conf spark.com.nec.native-csv=false \
     --conf spark.driver.resource.ve.amount=1 \
     --conf spark.driver.resource.ve.discoveryScript=/opt/spark/getVEsResources.sh \
     --conf spark.executor.resource.ve.amount=1 \
     --conf spark.executor.resource.ve.discoveryScript=/opt/spark/getVEsResources.sh \
+    --conf spark.com.nec.spark.kernel.precompiled=/opt/spark/work/egonzalez \
     --conf spark.executorEnv.VE_OMP_NUM_THREADS=1 \
-    --conf spark.executorEnv.VE_PROGINF=YES \
-    --conf spark.com.nec.spark.kernel.directory=/opt/spark/work/egonzalez \
     groups.py
+
+#    --conf spark.executorEnv.VE_PROGINF=YES \
+#        --conf spark.executor.extraJavaOptions=-agentpath:/opt/yjp/bin/linux-x86-64/libyjpagent.so \
+
