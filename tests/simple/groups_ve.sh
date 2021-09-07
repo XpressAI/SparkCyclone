c1e3f60bc08336aae1461a17d26cfdb80e52dcde
Eduardo Gonzalez <wm.eddie@gmail.com>
2021-09-07 11:32:40 +0900

    Make groups benchmark use 1 VE.

--- tests/simple/groups_ve.sh
+++ tests/simple/groups_ve.sh
@@ -6,13 +6,11 @@ $SPARK_HOME/bin/spark-submit \
     --deploy-mode cluster \
     --name groups.py_VE \
     --conf spark.com.nec.spark.ncc.path=/opt/nec/ve/bin/ncc \
-    --jars /opt/aurora4spark/aurora4spark-sql-plugin.jar \
-    --conf spark.executor.extraClassPath=/opt/aurora4spark/aurora4spark-sql-plugin.jar \
+    --jars /home/egonzalez/nec_spark/aurora4spark-remote/target/scala-2.12/aurora4spark-sql-plugin-assembly-0.1.0-SNAPSHOT.jar \
+    --conf spark.executor.extraClassPath=/home/egonzalez/nec_spark/aurora4spark-remote/target/scala-2.12/aurora4spark-sql-plugin-assembly-0.1.0-SNAPSHOT.jar \
     --conf spark.plugins=com.nec.spark.AuroraSqlPlugin \
     --conf spark.sql.columnVector.offheap.enabled=true \
     --conf spark.com.nec.native-csv=false \
-    --conf spark.driver.resource.ve.amount=1 \
-    --conf spark.driver.resource.ve.discoveryScript=/opt/spark/getVEsResources.sh \
     --conf spark.executor.resource.ve.amount=1 \
     --conf spark.executor.resource.ve.discoveryScript=/opt/spark/getVEsResources.sh \
     --conf spark.com.nec.spark.kernel.directory=/opt/spark/work/egonzalez \
