5a4132938c6c3e3cace768e885c1149e2f19796c
Wosin <blind.van@gmail.com>
2021-09-08 15:48:39 +0200

    Fix filterer.

--- src/main/resources/com/nec/arrow/functions/cpp/filterer.cc
+++ src/main/resources/com/nec/arrow/functions/cpp/filterer.cc
@@ -3,9 +3,9 @@
 #include "frovedis/dataframe/join.cc"
 #include <iostream>
 #include <vector>
-
-extern "C" long filter_doubles_over_15(non_null_double_vector* in,
-                                        non_null_double_vector* out)
+#include <cmath>
+extern "C" long filter_doubles_over_15(nullable_double_vector* in,
+                                        nullable_double_vector* out)
 {
     std::vector<double> in_vec_filtered;
     for ( long i = 0; i < in->count; i++ ) {
@@ -15,8 +15,13 @@ extern "C" long filter_doubles_over_15(non_null_double_vector* in,
     }
     double *out_data = (double *) malloc(in_vec_filtered.size() * sizeof(double));
     out->count = in_vec_filtered.size();
+
+    int validitySize = ceil(in_vec_filtered.size()/8.0);
     memcpy(out_data, in_vec_filtered.data(), out->count * sizeof(double));
     out->data = out_data;
-
+    out->validityBuffer = (unsigned char *)malloc(validitySize * sizeof(unsigned char*));
+    for(long i =0; i < validitySize; i++) {
+        out->validityBuffer[i] = 255;
+    }
     return 0;
 }
