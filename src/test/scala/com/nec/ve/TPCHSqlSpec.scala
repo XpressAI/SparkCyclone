8ffe40519e217144c94713c7be80a486def7ae2a
Eduardo Gonzalez <wm.eddie@gmail.com>
2021-09-27 23:57:20 +0900

    Recreate the views in each test.

--- src/test/scala/com/nec/ve/TPCHSqlSpec.scala
+++ src/test/scala/com/nec/ve/TPCHSqlSpec.scala
@@ -129,42 +129,39 @@ final class TPCHSqlSpec
     DynamicVeSqlExpressionEvaluationSpec.VeConfiguration
 
   def createViews(sparkSession: SparkSession): Unit = {
-    if (!initialized) {
-      import sparkSession.implicits._
+    import sparkSession.implicits._
 
-      val sc = sparkSession.sparkContext
-      val inputDir = "src/test/resources/dbgen/dbgen"
+    val sc = sparkSession.sparkContext
+    val inputDir = "src/test/resources/dbgen/dbgen"
 
-      val dfMap = Map(
-        "customer" -> sc.textFile(inputDir + "/customer.tbl*").map(_.split('|')).map(p =>
-          Customer(p(0).trim.toLong, p(1).trim, p(2).trim, p(3).trim.toLong, p(4).trim, p(5).trim.toDouble, p(6).trim, p(7).trim)).toDF(),
+    val dfMap = Map(
+      "customer" -> sc.textFile(inputDir + "/customer.tbl*").map(_.split('|')).map(p =>
+        Customer(p(0).trim.toLong, p(1).trim, p(2).trim, p(3).trim.toLong, p(4).trim, p(5).trim.toDouble, p(6).trim, p(7).trim)).toDF(),
 
-        "lineitem" -> sc.textFile(inputDir + "/lineitem.tbl*").map(_.split('|')).map(p =>
-          Lineitem(p(0).trim.toLong, p(1).trim.toLong, p(2).trim.toLong, p(3).trim.toLong, p(4).trim.toDouble, p(5).trim.toDouble, p(6).trim.toDouble, p(7).trim.toDouble, p(8).trim, p(9).trim, p(10).trim, p(11).trim, p(12).trim, p(13).trim, p(14).trim, p(15).trim)).toDF(),
+      "lineitem" -> sc.textFile(inputDir + "/lineitem.tbl*").map(_.split('|')).map(p =>
+        Lineitem(p(0).trim.toLong, p(1).trim.toLong, p(2).trim.toLong, p(3).trim.toLong, p(4).trim.toDouble, p(5).trim.toDouble, p(6).trim.toDouble, p(7).trim.toDouble, p(8).trim, p(9).trim, p(10).trim, p(11).trim, p(12).trim, p(13).trim, p(14).trim, p(15).trim)).toDF(),
 
-        "nation" -> sc.textFile(inputDir + "/nation.tbl*").map(_.split('|')).map(p =>
-          Nation(p(0).trim.toLong, p(1).trim, p(2).trim.toLong, p(3).trim)).toDF(),
+      "nation" -> sc.textFile(inputDir + "/nation.tbl*").map(_.split('|')).map(p =>
+        Nation(p(0).trim.toLong, p(1).trim, p(2).trim.toLong, p(3).trim)).toDF(),
 
-        "region" -> sc.textFile(inputDir + "/region.tbl*").map(_.split('|')).map(p =>
-          Region(p(0).trim.toLong, p(1).trim, p(2).trim)).toDF(),
+      "region" -> sc.textFile(inputDir + "/region.tbl*").map(_.split('|')).map(p =>
+        Region(p(0).trim.toLong, p(1).trim, p(2).trim)).toDF(),
 
-        "orders" -> sc.textFile(inputDir + "/orders.tbl*").map(_.split('|')).map(p =>
-          Order(p(0).trim.toLong, p(1).trim.toLong, p(2).trim, p(3).trim.toDouble, p(4).trim, p(5).trim, p(6).trim, p(7).trim.toLong, p(8).trim)).toDF(),
+      "orders" -> sc.textFile(inputDir + "/orders.tbl*").map(_.split('|')).map(p =>
+        Order(p(0).trim.toLong, p(1).trim.toLong, p(2).trim, p(3).trim.toDouble, p(4).trim, p(5).trim, p(6).trim, p(7).trim.toLong, p(8).trim)).toDF(),
 
-        "part" -> sc.textFile(inputDir + "/part.tbl*").map(_.split('|')).map(p =>
-          Part(p(0).trim.toLong, p(1).trim, p(2).trim, p(3).trim, p(4).trim, p(5).trim.toLong, p(6).trim, p(7).trim.toDouble, p(8).trim)).toDF(),
+      "part" -> sc.textFile(inputDir + "/part.tbl*").map(_.split('|')).map(p =>
+        Part(p(0).trim.toLong, p(1).trim, p(2).trim, p(3).trim, p(4).trim, p(5).trim.toLong, p(6).trim, p(7).trim.toDouble, p(8).trim)).toDF(),
 
-        "partsupp" -> sc.textFile(inputDir + "/partsupp.tbl*").map(_.split('|')).map(p =>
-          Partsupp(p(0).trim.toLong, p(1).trim.toLong, p(2).trim.toLong, p(3).trim.toDouble, p(4).trim)).toDF(),
+      "partsupp" -> sc.textFile(inputDir + "/partsupp.tbl*").map(_.split('|')).map(p =>
+        Partsupp(p(0).trim.toLong, p(1).trim.toLong, p(2).trim.toLong, p(3).trim.toDouble, p(4).trim)).toDF(),
 
-        "supplier" -> sc.textFile(inputDir + "/supplier.tbl*").map(_.split('|')).map(p =>
-          Supplier(p(0).trim.toLong, p(1).trim, p(2).trim, p(3).trim.toLong, p(4).trim, p(5).trim.toDouble, p(6).trim)).toDF()
-      )
+      "supplier" -> sc.textFile(inputDir + "/supplier.tbl*").map(_.split('|')).map(p =>
+        Supplier(p(0).trim.toLong, p(1).trim, p(2).trim, p(3).trim.toLong, p(4).trim, p(5).trim.toDouble, p(6).trim)).toDF()
+    )
 
-      dfMap.foreach {
-        case (key, value) => value.createOrReplaceTempView(key)
-      }
-      initialized = true
+    dfMap.foreach {
+      case (key, value) => value.createOrReplaceTempView(key)
     }
   }
 
