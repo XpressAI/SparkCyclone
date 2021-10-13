a795230d61f61cac56f69c2800d75abd03c5b720
WGipfeli <w@gipfeli.sg>
2021-10-13 22:25:29 +0100

    Debug UDP

--- src/test/scala/com/nec/cmake/functions/IncludeUDPDebugSpec.scala
+++ src/test/scala/com/nec/cmake/functions/IncludeUDPDebugSpec.scala
@@ -6,14 +6,16 @@ import org.scalatest.freespec.AnyFreeSpec
 
 final class IncludeUDPDebugSpec extends AnyFreeSpec {
   "It works" in {
-    val debugger = UdpDebug.Always("sock", "dest", "127.0.0.1", "12345")
-    val code = CodeLines.from(
-      debugger.headers,
-      s"void do() {",
-      CodeLines.from(debugger.createSock, debugger.send("\"test\""), debugger.close).indented,
-      "}"
-    )
+    if (!scala.util.Properties.isWin) {
+      val debugger = UdpDebug.Always("sock", "dest", "127.0.0.1", "12345")
+      val code = CodeLines.from(
+        debugger.headers,
+        s"void do() {",
+        CodeLines.from(debugger.createSock, debugger.send("\"test\""), debugger.close).indented,
+        "}"
+      )
 
-    CMakeBuilder.buildC(code.cCode, debug = true)
+      CMakeBuilder.buildC(code.cCode, debug = true)
+    }
   }
 }
