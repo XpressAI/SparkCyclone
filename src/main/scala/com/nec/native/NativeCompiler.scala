6dbdf3c0271d43cad61f10ed82eaadfa6176a433
Eduardo Gonzalez <wm.eddie@gmail.com>
2021-10-15 12:29:23 +0900

    Used previously compiled .so files if they exist.

--- src/main/scala/com/nec/native/NativeCompiler.scala
+++ src/main/scala/com/nec/native/NativeCompiler.scala
@@ -64,19 +64,26 @@ object NativeCompiler {
     extends NativeCompiler
     with LazyLogging {
     override def forCode(code: String): Path = {
-      logger.debug(s"Compiling for the VE...: $code")
-      val startTime = System.currentTimeMillis()
       val cc = combinedCode(code)
-      val soName =
-        VeKernelCompiler(
-          compilationPrefix = s"_spark_${cc.hashCode}",
-          Paths.get(buildDir),
-          veCompilerConfig
-        )
-          .compile_c(cc)
-      val endTime = System.currentTimeMillis() - startTime
-      logger.debug(s"Compiled code in ${endTime}ms to path ${soName}.")
-      soName
+      val sourcePath = Paths.get(buildDir).resolve(s"_spark_${cc.hashCode}.so").toAbsolutePath
+
+      if (sourcePath.toFile.exists()) {
+        logger.debug(s"Loading precompiled from path: $sourcePath")
+        sourcePath
+      } else {
+        logger.debug(s"Compiling for the VE...: $code")
+        val startTime = System.currentTimeMillis()
+        val soName =
+          VeKernelCompiler(
+            compilationPrefix = s"_spark_${cc.hashCode}",
+            Paths.get(buildDir),
+            veCompilerConfig
+          )
+            .compile_c(cc)
+        val endTime = System.currentTimeMillis() - startTime
+        logger.debug(s"Compiled code in ${endTime}ms to path ${soName}.")
+        soName
+      }
     }
   }
 
