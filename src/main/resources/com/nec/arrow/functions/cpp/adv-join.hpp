8f07ac078041047c350916d5de2108c7e4d9d804
Eduardo Gonzalez <wm.eddie@gmail.com>
2021-11-10 00:18:50 +0900

    strings joined properly.

--- src/main/resources/com/nec/arrow/functions/cpp/adv-join.hpp
+++ src/main/resources/com/nec/arrow/functions/cpp/adv-join.hpp
@@ -21,6 +21,10 @@ extern "C" long adv_join(
     nullable_int_vector *o_b,
     nullable_double_vector *o_c)
 {
+    // SELECT X.A, X.C, Y.C FROM X LEFT JOIN Y ON X.A = Y.A AND X.B = Y.B
+    // X = [A: String, B: Long, C: Int]
+    // Y = [A: String, B: Long, C: Double]
+
     frovedis::words left_words = varchar_vector_to_words(x_a);
     frovedis::words right_words = varchar_vector_to_words(y_a);
 
@@ -30,40 +34,37 @@ extern "C" long adv_join(
     frovedis::dict left_dict = frovedis::make_dict(left_words);
     frovedis::dict right_dict = frovedis::make_dict(right_words);
 
-    left_words.print();
-    std::cout << std::endl;
-    right_words.print();
-    std::cout << std::endl;
-
     std::vector<size_t> a = left_dict.lookup(right_cwords);
-
-    std::cout << "keys: ";
+    std::cout << "a = [";
     for (int i = 0; i < a.size(); i++) {
         std::cout << a[i] << ", ";
     }
-    std::cout << std::endl << "Results: " << std::endl;
+    std::cout << "]" << std::endl;
+
+    std::vector<size_t> b = right_dict.lookup(left_cwords);
+    std::cout << "b = [";
+    for (int i = 0; i < b.size(); i++) {
+        std::cout << b[i] << ", ";
+    }
+    std::cout << "]" << std::endl;
 
     frovedis::words result = left_dict.index_to_words(a);
-    result.print();
+    words_to_varchar_vector(result, o_a);
 
-    std::cout << std::endl;
-
-    words_to_varchar_vector(result, out_a);
-
-    out_b->count = a.size();
-    out_b->data = (int32_t *)malloc(out_b->count * sizeof(int32_t));
-    out_b->validityBuffer = (uint64_t *)malloc(ceil(out_b->count / 64.0) * sizeof(uint64_t));
+    o_b->count = a.size();
+    o_b->data = (int32_t *)malloc(o_b->count * sizeof(int32_t));
+    o_b->validityBuffer = (uint64_t *)malloc(ceil(o_b->count / 64.0) * sizeof(uint64_t));
     for (int i = 0; i < a.size(); i++) {
-        out_b->data[i] = x_c->data[a[i]];
-        set_validity(out_b->validityBuffer, i, check_valid(x_c->validityBuffer, a[i]));
+        o_b->data[i] = x_c->data[a[i]];
+        set_validity(o_b->validityBuffer, i, check_valid(x_c->validityBuffer, a[i]));
     }
 
-    out_c->count = a.size();
-    out_c->data = (double *)malloc(out_c->count * sizeof(double));
-    out_c->validityBuffer = (uint64_t *)malloc(ceil(out_c->count / 64.0) * sizeof(uint64_t));
+    o_c->count = a.size();
+    o_c->data = (double *)malloc(o_c->count * sizeof(double));
+    o_c->validityBuffer = (uint64_t *)malloc(ceil(o_c->count / 64.0) * sizeof(uint64_t));
     for (int i = 0; i < a.size(); i++) {
-        out_c->data[i] = y_c->data[a[i]];
-        set_validity(out_b->validityBuffer, i, check_valid(y_c->validityBuffer, a[i]));
+        o_c->data[i] = y_c->data[a[i]];
+        set_validity(o_c->validityBuffer, i, check_valid(y_c->validityBuffer, a[i]));
     }
 
     return 0;
