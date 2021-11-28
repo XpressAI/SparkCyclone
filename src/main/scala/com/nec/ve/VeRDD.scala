725b03447efb72836d7b1b8cfb2f7db594a2efa8
Wosin <blind.van@gmail.com>
2021-11-28 17:59:00 +0100

    Test.

--- src/main/scala/com/nec/ve/VeRDD.scala
+++ src/main/scala/com/nec/ve/VeRDD.scala
@@ -21,12 +21,13 @@ object VeRDD {
 
   def exchangeL(
     rdd: RDD[(Int, List[VeColVector])]
-  ): RDD[List[VeColVector]] =
+  ): RDD[List[VeColVector]] = {
+    import com.nec.spark.SparkCycloneExecutorPlugin.veProcess
+
     rdd
       .mapPartitions(
         f = iter =>
           iter.map { case (p, v) =>
-            import com.nec.spark.SparkCycloneExecutorPlugin.veProcess
             (p, (v, v.map(_.serialize())))
           },
         preservesPartitioning = true
@@ -34,11 +35,11 @@ object VeRDD {
       .sortByKey()
       .mapPartitions(
         f = iter => {
-          import com.nec.spark.SparkCycloneExecutorPlugin.veProcess
           iter.map { case (_, (v, ba)) => v.zip(ba).map { case (vv, bb) => vv.deserialize(bb) } }
         },
         preservesPartitioning = true
       )
+  }
 
   implicit class RichKeyedRDD(rdd: RDD[(Int, VeColVector)]) {
     def exchangeBetweenVEs()(implicit veProcess: VeProcess): RDD[VeColVector] = exchange(rdd)
