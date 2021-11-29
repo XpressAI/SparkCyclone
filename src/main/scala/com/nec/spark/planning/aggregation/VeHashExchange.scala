c4f39c49906fc4da465a8d0b7c190afd7a849622
WGipfeli <w@gipfeli.sg>
2021-11-29 09:00:12 +0900

    exchange, try this

--- src/main/scala/com/nec/spark/planning/aggregation/VeHashExchange.scala
+++ src/main/scala/com/nec/spark/planning/aggregation/VeHashExchange.scala
@@ -43,8 +43,8 @@ case class VeHashExchange(exchangeFunction: VeFunction, child: SparkPlan)
         }
       }
     }
-    // .exchangeBetweenVEs()
-    .exchangeBetweenVEsNoSer()
+    .exchangeBetweenVEs()
+    // .exchangeBetweenVEsNoSer()
     .mapPartitions(f = _.map(lv => VeColBatch.fromList(lv)), preservesPartitioning = true)
 
   override def output: Seq[Attribute] = child.output
