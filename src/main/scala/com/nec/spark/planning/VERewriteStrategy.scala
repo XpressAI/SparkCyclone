6fcda651a94148998c173b2745edd2109dac1ad5
WGipfeli <w@gipfeli.sg>
2021-12-04 13:12:48 +0000

    Disable useVeExchange

--- src/main/scala/com/nec/spark/planning/VERewriteStrategy.scala
+++ src/main/scala/com/nec/spark/planning/VERewriteStrategy.scala
@@ -405,7 +405,7 @@ final case class VERewriteStrategy(
               )
           } yield {
 
-            val useVeExchange = true
+            val useVeExchange = false
             val exchangePlan =
               if (useVeExchange)
                 VeHashExchange(
