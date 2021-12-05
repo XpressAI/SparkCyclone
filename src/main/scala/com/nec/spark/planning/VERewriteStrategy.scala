5abb7f93200d48f4b010a6025078e1d9e302bf06
WGipfeli <w@gipfeli.sg>
2021-12-06 05:37:39 +0900

    enable ve exchange

--- src/main/scala/com/nec/spark/planning/VERewriteStrategy.scala
+++ src/main/scala/com/nec/spark/planning/VERewriteStrategy.scala
@@ -412,7 +412,7 @@ final case class VERewriteStrategy(
               )
           } yield {
 
-            val useVeExchange = false
+            val useVeExchange = true
             val exchangePlan =
               if (useVeExchange)
                 VeHashExchange(
