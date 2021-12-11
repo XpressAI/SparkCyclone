29ce57fe7d1831b7218a17d6e56429d0160b6629
WGipfeli <w@gipfeli.sg>
2021-12-11 18:12:17 +0000

    More extractions

--- src/main/scala/com/nec/spark/agile/join/GenericJoiner.scala
+++ src/main/scala/com/nec/spark/agile/join/GenericJoiner.scala
@@ -23,6 +23,18 @@ object GenericJoiner {
     val o_c = "o_c"
     val conj_x = "conj_x"
     val conj_y = "conj_y"
+    val a1_idx = "a1_idx"
+    val a2_idx = "a2_idx"
+    val a1_out = "a1_out"
+    val b1_idx = "b1_idx"
+    val a2_out = "a2_out"
+    val b2_idx = "b2_idx"
+    val b1_out = "b1_out"
+    val b2_out = "b2_out"
+    val a1 = "a1"
+    val a2 = "a2"
+    val b1 = "b1"
+    val b2 = "b2"
     CodeLines.from(
       """#include "frovedis/core/radix_sort.hpp"""",
       """#include "frovedis/dataframe/join.hpp"""",
@@ -71,41 +83,44 @@ object GenericJoiner {
           s"""frovedis::dict ${x_a_dict} = frovedis::make_dict(${x_a_words});""",
           """""",
           """// First = => a1 = a2""",
-          s"""std::vector<size_t> a1 = ${x_a_dict}.lookup(${x_a_cwords});""",
-          """std::vector<size_t> a1_idx(a1.size());""",
-          CodeLines.forLoop("i", "a1.size()")(CodeLines.from("""a1_idx[i] = i;""")),
-          s"""std::vector<size_t> a2 = ${x_a_dict}.lookup(${y_a_cwords});""",
-          """std::vector<size_t> a2_idx(a2.size());""",
-          CodeLines.forLoop("i", "a2.size()")(CodeLines.from("""a2_idx[i] = i;""")),
-          """std::vector<size_t> a1_out;""",
-          """std::vector<size_t> a2_out;""",
-          """frovedis::equi_join(a2, a2_idx, a1, a1_idx, a2_out, a1_out);""",
-          s"""std::vector<int64_t> b1(${x_b}->count);""",
-          s"""std::vector<size_t> b1_idx(${x_b}->count);""",
+          s"""std::vector<size_t> ${a1} = ${x_a_dict}.lookup(${x_a_cwords});""",
+          s"""std::vector<size_t> ${a1_idx}(${a1}.size());""",
+          CodeLines.forLoop("i", "a1.size()")(CodeLines.from(s"""${a1_idx}[i] = i;""")),
+          s"""std::vector<size_t> ${a2} = ${x_a_dict}.lookup(${y_a_cwords});""",
+          s"""std::vector<size_t> ${a2_idx}(${a2}.size());""",
+          CodeLines.forLoop("i", s"${a2}.size()")(CodeLines.from(s"""${a2_idx}[i] = i;""")),
+          s"""std::vector<size_t> ${a1_out};""",
+          s"""std::vector<size_t> ${a2_out};""",
+          s"""frovedis::equi_join(${a2}, ${a2_idx}, ${a1}, ${a1_idx}, ${a2_out}, ${a1_out});""",
+          s"""std::vector<int64_t> ${b1}(${x_b}->count);""",
+          s"""std::vector<size_t> ${b1_idx}(${x_b}->count);""",
           CodeLines.forLoop(counterName = "i", until = s"${x_b}->count")(
-            CodeLines.from(s"""b1[i] = ${x_b}->data[i];""", """b1_idx[i] = i;""")
+            CodeLines.from(s"""${b1}[i] = ${x_b}->data[i];""", s"""${b1_idx}[i] = i;""")
           ),
-          s"""std::vector<int64_t> b2(${y_b}->count);""",
-          s"""std::vector<size_t> b2_idx(${y_b}->count);""",
+          s"""std::vector<int64_t> ${b2}(${y_b}->count);""",
+          s"""std::vector<size_t> ${b2_idx}(${y_b}->count);""",
           CodeLines.forLoop(counterName = "i", until = s"${y_b}->count")(
-            CodeLines.from(s"""b2[i] = ${y_b}->data[i];""", """b2_idx[i] = i;""")
+            CodeLines.from(s"""b2[i] = ${y_b}->data[i];""", s"""${b2_idx}[i] = i;""")
           ),
-          """std::vector<size_t> b1_out;""",
-          """std::vector<size_t> b2_out;""",
-          """frovedis::equi_join(b2, b2_idx, b1, b1_idx, b2_out, b1_out);""",
+          s"""std::vector<size_t> ${b1_out};""",
+          s"""std::vector<size_t> ${b2_out};""",
+          s"""frovedis::equi_join(${b2}, ${b2_idx}, ${b1}, ${b1_idx}}, ${b2_out}, ${b1_out});""",
           s"""std::vector<size_t> ${conj_x};""",
           s"""std::vector<size_t> ${conj_y};""",
-          CodeLines.forLoop(counterName = "i", until = "a1_out.size()")(
-            CodeLines.forLoop(counterName = "j", until = "b1_out.size()")(
-              CodeLines.ifStatement(condition = """a1_out[i] == b1_out[j]""")(
-                CodeLines.ifStatement(condition = """a2_out[i] == b2_out[j]""")(
+          CodeLines.forLoop(counterName = "i", until = s"${a1_out}.size()")(
+            CodeLines.forLoop(counterName = "j", until = s"${b1_out}.size()")(
+              CodeLines.ifStatement(condition = s"""${a1_out}[i] == ${b1_out}[j]""")(
+                CodeLines.ifStatement(condition = s"""${a2_out}[i] == ${b2_out}[j]""")(
                   CodeLines
-                    .from(s"""${conj_x}.push_back(a1_out[i]);""", s"""${conj_y}.push_back(a2_out[i]);""")
+                    .from(
+                      s"""${conj_x}.push_back(${a1_out}[i]);""",
+                      s"""${conj_y}.push_back(${a2_out}[i]);"""
+                    )
                 )
               )
             )
           ),
-          s"""frovedis::words ${o_a_words} = ${x_a_dict}.index_to_words(a2);""",
+          s"""frovedis::words ${o_a_words} = ${x_a_dict}.index_to_words(${a2});""",
           s"""words_to_varchar_vector(${o_a_words}, ${o_a});""",
           GroupByOutline
             .initializeScalarVector(VeScalarType.VeNullableInt, s"${o_b}", s"${conj_x}.size()"),
