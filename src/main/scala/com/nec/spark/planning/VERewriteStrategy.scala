bcca741b895df3698986707409f38fd9f79211cc
Wosin <blind.van@gmail.com>
2021-12-14 13:40:12 +0100

    Skip identity operations in OneStagePlan.

--- src/main/scala/com/nec/spark/planning/VERewriteStrategy.scala
+++ src/main/scala/com/nec/spark/planning/VERewriteStrategy.scala
@@ -155,9 +155,14 @@ final case class VERewriteStrategy(
 
         case logical.Project(projectList, child) if projectList.nonEmpty && options.projectOnVe =>
           implicit val fallback: EvalFallback = EvalFallback.noOp
-
+          val nonIdentityProjections = projectList
+            .toList
+            .filter {
+              case AttributeReference(_, _, _, _) => false
+              case _ => true
+            }
           val planE = for {
-            outputs <- projectList.toList.zipWithIndex.map { case (att, idx) =>
+            outputs <- nonIdentityProjections.zipWithIndex.map { case (att, idx) =>
               val referenced = replaceReferences(InputPrefix, plan.inputSet.toList, att)
               if (referenced.dataType == StringType)
                 evalString(referenced).map(stringProducer =>
