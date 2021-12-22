4aee003ab085ff83c974087e3018f4d6c955e1c2
WGipfeli <w@gipfeli.sg>
2021-12-22 00:01:39 +0000

    Vertical-align all the cells

--- tpcbench-run/src/main/scala/sc/RunDatabase.scala
+++ tpcbench-run/src/main/scala/sc/RunDatabase.scala
@@ -119,7 +119,10 @@ object RunDatabase {
           """<link rel="stylesheet" href="https://unpkg.com/purecss@2.0.6/build/pure-min.css" integrity="sha384-Uu6IeWbM+gzNVXJcM9XV3SohHtmWE+3VGi496jvgX1jyvDTXfdK+rfZc8C1Aehk5" crossorigin="anonymous">"""
         ),
         raw("""<meta name="viewport" content="width=device-width, initial-scale=1">"""),
-        raw("""<style>body {font-size:0.8em; }</style>""".stripMargin)
+        raw(
+          """<style>body {font-size:0.8em; }
+            |td {vertical-align:top; }
+            |</style>""".stripMargin)
       ),
       body(
         table(
