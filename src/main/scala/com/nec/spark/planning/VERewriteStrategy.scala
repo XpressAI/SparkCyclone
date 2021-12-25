70c59583f22d920014f138471c30046810f8d753
WGipfeli <w@gipfeli.sg>
2021-12-25 18:50:35 +0000

    Fix serializer bug

--- src/main/scala/com/nec/spark/planning/VERewriteStrategy.scala
+++ src/main/scala/com/nec/spark/planning/VERewriteStrategy.scala
@@ -94,9 +94,6 @@ final case class VERewriteStrategy(
       log.debug(
         s"Processing input plan with VERewriteStrategy: $plan, output types were: ${plan.output.map(_.dataType)}"
       )
-      println(
-        s"Processing input plan with VERewriteStrategy: $plan, output types were: ${plan.output.map(_.dataType)}"
-      )
 
       def res: immutable.Seq[SparkPlan] = plan match {
         case imr @ InMemoryRelation(output, cb, oo)
