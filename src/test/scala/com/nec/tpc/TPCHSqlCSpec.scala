cc2054941b33bbb4c9e9305bfb5dc4f51b7be056
WGipfeli <w@gipfeli.sg>
2021-12-25 23:55:44 +0000

    Disable ignore

--- src/test/scala/com/nec/tpc/TPCHSqlCSpec.scala
+++ src/test/scala/com/nec/tpc/TPCHSqlCSpec.scala
@@ -242,7 +242,7 @@ abstract class TPCHSqlCSpec
   def withTpchViews[T](
     appName: String,
     configure: SparkSession.Builder => SparkSession.Builder,
-    ignore: Boolean = true
+    ignore: Boolean = false
   )(f: SparkSession => T): Unit =
     if (ignore) { appName ignore {} }
     else {
