94ffa3c4f19cac415464b5df99cf2bc502b62b83
WGipfeli <w@gipfeli.sg>
2021-12-26 19:39:45 +0000

    Fix up to use a random port

--- tpcbench-run/src/test/scala/sc/LogbackOverSocketTest.scala
+++ tpcbench-run/src/test/scala/sc/LogbackOverSocketTest.scala
@@ -5,22 +5,19 @@ import cats.effect.unsafe.implicits.global
 import ch.qos.logback.classic.Level
 import ch.qos.logback.classic.net.SocketAppender
 import ch.qos.logback.core.ConsoleAppender
-import com.nec.tracing.TracingListenerApp.{serverHost, serverPort, socketToLines}
-import fs2.io.net.Network
 import com.comcast.ip4s.{Host, Port}
 import fs2.concurrent.SignallingRef
+import fs2.io.net.Network
 import org.scalatest.freespec.AnyFreeSpec
 import org.slf4j.LoggerFactory
 
-import java.io.ObjectInputStream
 import scala.concurrent.duration.DurationInt
 
 final class LogbackOverSocketTest extends AnyFreeSpec {
   "It works" in {
-    import ch.qos.logback.classic.LoggerContext
+    import ch.qos.logback.classic.{Logger, LoggerContext}
     import ch.qos.logback.classic.encoder.PatternLayoutEncoder
     import ch.qos.logback.classic.spi.ILoggingEvent
-    import ch.qos.logback.classic.Logger
 
     val lc = LoggerFactory.getILoggerFactory.asInstanceOf[LoggerContext]
     val ple = new PatternLayoutEncoder
@@ -35,47 +32,37 @@ final class LogbackOverSocketTest extends AnyFreeSpec {
 
     val completion = SignallingRef.of[IO, Boolean](false).unsafeRunSync()
 
-    val qq = Network[IO]
-      .serverResource(address = Host.fromString("127.0.0.1"), port = Port.fromInt(9991))
+    Network[IO]
+      .serverResource(address = Host.fromString("127.0.0.1"))
       .use { case (socketAddress, socketStream) =>
-        socketStream
-          .flatMap(socket =>
-            fs2.Stream
-              .resource(fs2.io.toInputStreamResource(socket.reads))
-              .map(inputStream => new ObjectInputStream(inputStream))
-              .interruptWhen(haltWhenTrue = completion)
-              .flatMap(ois =>
-                fs2.Stream
-                  .repeatEval(IO.blocking(ois.readObject().asInstanceOf[ILoggingEvent]))
-                  .handleErrorWith {
-                    case _: java.io.EOFException => fs2.Stream.empty
-                    case other                   => throw other
-                  }
-              )
-              .evalMap(e => IO.println(e.toString))
-          )
+        val sr = socketStream
+          .flatMap(socket => LogbackListener.getLoggingEvents(socket))
           .interruptWhen(haltWhenTrue = completion)
           .compile
-          .drain
+          .toList
+
+        val stt = sr.start.unsafeRunSync()
+
+        val socketAppender = new SocketAppender()
+        socketAppender.setContext(lc)
+        socketAppender.setRemoteHost("127.0.0.1")
+        socketAppender.setPort(socketAddress.port.value)
+        socketAppender.start()
+
+        val logger: Logger = LoggerFactory.getLogger(this.getClass).asInstanceOf[Logger]
+        logger.addAppender(consoleAppender)
+        logger.addAppender(socketAppender)
+        logger.setLevel(Level.DEBUG)
+        logger.setAdditive(false)
+        logger.info("It works?")
+        logger.detachAppender(socketAppender)
+        socketAppender.stop()
+        completion.set(true).delayBy(2.seconds).unsafeRunSync()
+        val res = stt.joinWithNever.unsafeRunSync()
+        val messages = res.map(_.getMessage)
+        IO.delay(assert(messages.contains("It works?")))
       }
+      .unsafeRunSync()
 
-    val stt = qq.start.unsafeRunSync()
-
-    val socketAppender = new SocketAppender()
-    socketAppender.setContext(lc)
-    socketAppender.setRemoteHost("127.0.0.1")
-    socketAppender.setPort(9991)
-    socketAppender.start()
-
-    val logger: Logger = LoggerFactory.getLogger(this.getClass).asInstanceOf[Logger]
-    logger.addAppender(consoleAppender)
-    logger.addAppender(socketAppender)
-    logger.setLevel(Level.DEBUG)
-    logger.setAdditive(false)
-    logger.info("It works?")
-    logger.detachAppender(socketAppender)
-    socketAppender.stop()
-    completion.set(true).delayBy(2.seconds).unsafeRunSync()
-    stt.joinWithNever.unsafeRunSync()
   }
 }
