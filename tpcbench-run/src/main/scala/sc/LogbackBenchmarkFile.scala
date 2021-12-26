e5ff29de2bdb3d3f1400eadf4021264a63a1ccf9
WGipfeli <w@gipfeli.sg>
2021-12-26 19:57:39 +0000

    No $$

--- tpcbench-run/src/main/scala/sc/LogbackBenchmarkFile.scala
+++ tpcbench-run/src/main/scala/sc/LogbackBenchmarkFile.scala
@@ -4,7 +4,7 @@ object LogbackBenchmarkFile {
   def forPort(port: Int): scala.xml.Elem = <configuration debug="true">
     <appender name="SERVER" class="ch.qos.logback.classic.net.server.SocketAppender">
       <remoteHost>127.0.0.1</remoteHost>
-      <port>${port}</port>
+      <port>{port}</port>
       <reconnectionDelay>10000</reconnectionDelay>
       <includeCallerData>true</includeCallerData>
     </appender>
