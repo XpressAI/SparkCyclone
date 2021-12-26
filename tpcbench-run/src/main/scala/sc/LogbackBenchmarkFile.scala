a0cfa77740f7858f9caf57ab12f3f631b322fb94
WGipfeli <w@gipfeli.sg>
2021-12-26 19:57:08 +0000

    Fix up, shoudl be client

--- tpcbench-run/src/main/scala/sc/LogbackBenchmarkFile.scala
+++ tpcbench-run/src/main/scala/sc/LogbackBenchmarkFile.scala
@@ -2,8 +2,10 @@ package sc
 
 object LogbackBenchmarkFile {
   def forPort(port: Int): scala.xml.Elem = <configuration debug="true">
-    <appender name="SERVER" class="ch.qos.logback.classic.net.server.ServerSocketAppender">
+    <appender name="SERVER" class="ch.qos.logback.classic.net.server.SocketAppender">
+      <remoteHost>127.0.0.1</remoteHost>
       <port>${port}</port>
+      <reconnectionDelay>10000</reconnectionDelay>
       <includeCallerData>true</includeCallerData>
     </appender>
     <root level="info">
