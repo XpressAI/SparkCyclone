d3081e7819eb94ee98f25b9e621e3780acea67cd
Wosin <blind.van@gmail.com>
2022-01-20 02:35:46 +0100

    Changes.

--- tests/tpchbench/run_ve.sh
+++ tests/tpchbench/run_ve.sh
@@ -4,18 +4,17 @@ export SPARK_HOME=/opt/spark
 
 time $SPARK_HOME/bin/spark-submit \
     --master yarn \
-    --num-executors=8 --executor-cores=1 --executor-memory=8G \
+    --num-executors=1 --executor-cores=1 --executor-memory=8G \
     --deploy-mode cluster \
     --name TPC-H_VE_$1 \
     --conf spark.com.nec.spark.ncc.path=/opt/nec/ve/bin/ncc \
-    --jars /home/egonzalez/nec_spark/aurora4spark-remote/target/scala-2.12/spark-cyclone-sql-plugin-assembly-0.1.0-SNAPSHOT.jar \
-    --conf spark.executor.extraClassPath=/home/egonzalez/nec_spark/aurora4spark-remote/target/scala-2.12/spark-cyclone-sql-plugin-assembly-0.1.0-SNAPSHOT.jar \
+    --jars /opt/cyclone/spark-cyclone-sql-plugin.jar \
+    --conf spark.executor.extraClassPath=/opt/cyclone/spark-cyclone-sql-plugin.jar \
     --conf spark.plugins=com.nec.spark.AuroraSqlPlugin \
     --conf spark.sql.columnVector.offheap.enabled=true \
     --conf spark.com.nec.spark.ve.columnBatchSize=500000 \
     --conf spark.executor.resource.ve.amount=1 \
     --conf spark.executor.resource.ve.discoveryScript=/opt/spark/getVEsResources.sh \
-    --conf spark.com.nec.spark.kernel.directory=/opt/spark/work/cyclone \
     --conf spark.executorEnv.VE_OMP_NUM_THREADS=1 \
     --conf spark.com.nec.spark.sort-on-ve=true \
     --conf spark.com.nec.spark.filter-on-ve=true \
