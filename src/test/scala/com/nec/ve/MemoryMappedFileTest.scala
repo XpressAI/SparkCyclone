8be82af6416e1456a7bc7c66dc68736342a54f3f
wgif <52247468+wgip@users.noreply.github.com>
2022-01-23 19:29:50 +0000

    Simplify more. Don't need threading here

--- src/test/scala/com/nec/ve/MemoryMappedFileTest.scala
+++ src/test/scala/com/nec/ve/MemoryMappedFileTest.scala
@@ -14,60 +14,42 @@ final class MemoryMappedFileTest extends AnyFreeSpec {
   val FILE_SIZE = 1000L
 
   def testFlow(writerM: => SharedMemory, readerM: => SharedMemory): Unit = {
-    try {
-      val LIMIT = 0;
-      val COMMIT = 8;
-      val DATA = 16;
-      var wM: SharedMemory = writerM
-      val writer = new Thread() {
-        override def run(): Unit = {
-          try {
-            Thread.sleep(500)
-            wM.putLongVolatile(LIMIT, 1)
-            Thread.sleep(500)
-            wM.putLong(DATA, 2)
-            wM.putLongVolatile(COMMIT, 1)
-          } catch {
-            case e: Throwable =>
-              e.printStackTrace()
-          }
-        }
-      }
-      writer.start()
+    val LIMIT = 0
+    val COMMIT = 8
+    val DATA = 16
+    val wM: SharedMemory = writerM
+    wM.putLongVolatile(LIMIT, 1)
+    wM.putLong(DATA, 2)
+    wM.putLongVolatile(COMMIT, 1)
 
-      try {
-        val m: SharedMemory = readerM
-        var limit = m.getLong(LIMIT)
-        assert(limit == 0)
-        var break = false
-        while (!break) {
-          limit = m.getLongVolatile(LIMIT)
-          if (limit != 0) {
-            assert(limit == 1)
-            break = true
-          }
-        }
-        var commit = m.getLongVolatile(COMMIT)
-        var data = m.getLong(DATA)
-        assert(commit == 0)
-        assert(data == 0)
-        break = false
-        while (!break) {
-          commit = m.getLongVolatile(COMMIT)
-          if (commit != 0) {
-            assert(commit == 1)
-            break = true
-          }
-        }
-        data = m.getLong(DATA)
-        try assert(data == 2)
-        finally {
-          m.unmap()
-          wM.unmap()
-        }
-      } finally {
-        writer.join()
+    val m: SharedMemory = readerM
+    var limit = m.getLong(LIMIT)
+    assert(limit == 0)
+    var break = false
+    while (!break) {
+      limit = m.getLongVolatile(LIMIT)
+      if (limit != 0) {
+        assert(limit == 1)
+        break = true
       }
+    }
+    var commit = m.getLongVolatile(COMMIT)
+    var data = m.getLong(DATA)
+    assert(commit == 0)
+    assert(data == 0)
+    break = false
+    while (!break) {
+      commit = m.getLongVolatile(COMMIT)
+      if (commit != 0) {
+        assert(commit == 1)
+        break = true
+      }
+    }
+    data = m.getLong(DATA)
+    try assert(data == 2)
+    finally {
+      m.unmap()
+      wM.unmap()
     }
   }
 
