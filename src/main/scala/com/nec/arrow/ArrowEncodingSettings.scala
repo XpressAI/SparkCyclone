2cfe0689bf0df03267c00f08937dee4b654bf50b
wgif <52247468+wgip@users.noreply.github.com>
2022-02-01 23:45:14 +0000

    Improve BatchAmplifier & docs
    - Use Mb measurement by default
    - Document all the Cyclone options

--- src/main/scala/com/nec/arrow/ArrowEncodingSettings.scala
+++ src/main/scala/com/nec/arrow/ArrowEncodingSettings.scala
@@ -4,10 +4,11 @@ import org.apache.spark.SparkContext
 import org.apache.spark.sql.internal.SQLConf
 
 /** This is done as [[SqlConf]] cannot be serialized. */
-final case class ArrowEncodingSettings(timezone: String, numRows: Int, batchSizeTargetBytes: Int)
+final case class ArrowEncodingSettings(timezone: String, numRows: Int, batchSizeTargetBytes: Long)
 
 object ArrowEncodingSettings {
-  def fromConf(conf: SQLConf)(implicit sparkContext: SparkContext): ArrowEncodingSettings =
+  def fromConf(conf: SQLConf)(implicit sparkContext: SparkContext): ArrowEncodingSettings = {
+
     ArrowEncodingSettings(
       timezone =
         try conf.sessionLocalTimeZone
@@ -15,16 +16,19 @@ object ArrowEncodingSettings {
           case _: Throwable => "UTC"
         },
       numRows = sparkContext.getConf
-        .getOption("com.nec.spark.ve.columnBatchSize")
+        .getOption("spark.com.nec.spark.ve.columnBatchSize")
         .map(_.toInt)
         .getOrElse(conf.columnBatchSize),
       batchSizeTargetBytes = sparkContext.getConf
-        .getOption("com.nec.spark.ve.targetBatchSizeKb")
-        .map(_.toInt)
-        .map(_ * 1024)
+        .getOption("spark.com.nec.spark.ve.targetBatchSizeMb")
+        .map(_.toLong)
+        .map(_ * Mb)
         .getOrElse(DefaultTargetBatchSize)
     )
+  }
 
+  private val Mb: Long = 1024 * 1024
   // 64M
-  private val DefaultTargetBatchSize: Int = 64 * 1024 * 1024
+  private val DefaultTargetBatchSize: Long = 64 * Mb
+
 }
