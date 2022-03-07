254afde797ba135252d26d2957ce7a0825bb9411
Paul Dubs <paul.dubs@gmail.com>
2022-03-07 19:32:19 +0100

    Try different way of setting up inputs

--- src/main/scala/com/nec/ve/VeProcess.scala
+++ src/main/scala/com/nec/ve/VeProcess.scala
@@ -527,14 +527,18 @@ object VeProcess {
       /** Output count of rows - better to know this in advance */
       veo.veo_args_set_i32(our_args, 1, batches.rows)
 
-      batches.groupedColumns.zipWithIndex.foreach { case (colGroup, index) =>
+      // Setup input pointers, such that each input pointer points to a batch of columns
+      batches.batches.head.cols.indices.foreach { cIdx =>
         val byteSize = 8 * batches.batches.size
         val lp = new LongPointer(batches.batches.size)
-        colGroup.relatedColumns.zipWithIndex.foreach { case (col, idx) =>
-          lp.put(idx, col.containerLocation)
+
+        batches.batches.zipWithIndex.foreach{ case (b, bIdx) =>
+          lp.put(bIdx, b.cols(cIdx).containerLocation)
         }
-        veo.veo_args_set_stack(our_args, 0, 2 + index, new BytePointer(lp), byteSize)
+
+        veo.veo_args_set_stack(our_args, 0, 2 + cIdx, new BytePointer(lp), byteSize)
       }
+
       val outPointers = results.map { veType =>
         val lp = new LongPointer(batches.batches.size)
         lp.put(-118)
