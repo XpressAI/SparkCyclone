7c2040ded3d09f14c896cbd5919fb709bb91d1dd
Marko Nußbaum <marko.nussbaum@maconus.com>
2022-03-09 19:21:27 +0100

    adds logging of times

--- rddbench/src/main/scala/RDDBench.scala
+++ rddbench/src/main/scala/RDDBench.scala
@@ -4,16 +4,35 @@ import org.apache.spark.rdd._
 object RDDBench {
 
   var sc: SparkContext = null
+  var timings: Map[String, Long] = Map()
 
   def main(args: Array[String]): Unit = {
     println("RDD Benchmark")
 
     setup()
 
-    bench01cpu()
+    benchmark("01 - CPU",  bench01cpu)
 
+    dumpResult()
     finishUp()
   }
+
+  def benchmark(title: String, f: () => Unit) = {
+    val ts_start = System.currentTimeMillis()
+    f()
+    val ts_end = System.currentTimeMillis()
+    val diff = ts_end - ts_start
+    timings = timings + (title -> diff)
+  }
+
+  def dumpResult() = {
+    println("======== RESULTS ===========")
+    for ( (title, duration) <- timings) {
+      println(title + "\t" + duration + "ms")
+    }
+    println("============================")
+  }
+
 
   def bench01cpu() = {
     val numbers = Array(10, 17, 23, 1, 51, 23, 15, 18, 19, 22, 12, 38, 17)
