20ae2ceb50f5ea2072f7ace86195a92c50b041c5
Marko Nußbaum <marko.nussbaum@maconus.com>
2022-03-14 10:30:21 +0100

    adds start script to run on Spark cluster

---
+++ rddbench/run_rddbench.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+export SPARK_HOME=/opt/spark
+
+time $SPARK_HOME/bin/spark-submit \
+    --master yarn \
+    --num-executors=8 --executor-cores=1 --executor-memory=1G \
+    --deploy-mode cluster \
+    --name RDDBench \
+    --conf spark.com.nec.spark.ncc.path=/opt/nec/ve/bin/ncc \
+    --jars /opt/cyclone/spark-cyclone-sql-plugin.jar \
+    --conf spark.executor.extraClassPath=/opt/cyclone/spark-cyclone-sql-plugin.jar \
+    --conf spark.plugins=com.nec.spark.AuroraSqlPlugin \
+    --conf spark.sql.columnVector.offheap.enabled=true \
+    --conf spark.executor.resource.ve.amount=1 \
+    --conf spark.executor.resource.ve.discoveryScript=/opt/spark/getVEsResources.sh \
+    --conf spark.com.nec.spark.kernel.directory=/opt/spark/work/cyclone \
+    --conf spark.executorEnv.VE_OMP_NUM_THREADS=1 \
+    target/scala-2.12/rddbench_2.12-0.1.jar \
+    $*
