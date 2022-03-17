9c915d2b795c1ee0333a9435f88b9c40ceaa0126
Eduardo Gonzalez <wm.eddie@gmail.com>
2022-03-18 00:11:20 +0900

    Add implementation of simple evalFunction.

--- src/main/scala/com/nec/ve/VeRDD.scala
+++ src/main/scala/com/nec/ve/VeRDD.scala
@@ -2,13 +2,19 @@ package com.nec.ve
 
 import com.nec.native.CppTranspiler
 import com.nec.spark.SparkCycloneDriverPlugin
+import com.nec.spark.agile.CFunctionGeneration.CVector
+import com.nec.spark.agile.core.CFunction2
+import com.nec.ve.VeProcess.OriginalCallingContext
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{Partition, TaskContext}
 
+import java.nio.file.{Path, Paths}
+import java.time.Instant
 import scala.language.implicitConversions
 import scala.reflect.ClassTag
 
-class VeRDD[T](rdd: RDD[T]) extends RDD[T](rdd) {
+class VeRDD[T: ClassTag](rdd: RDD[T]) extends RDD[T](rdd) {
+  var func: CFunction2 = _
 
   import scala.reflect.runtime.universe._
 
@@ -59,9 +65,29 @@ class VeRDD[T](rdd: RDD[T]) extends RDD[T](rdd) {
   override protected def getPartitions: Array[Partition] = {
     Array() // TODO
   }
+
+  def withCompiled[U](cCode: String)(f: Path => U): U = {
+    val veBuildPath = Paths.get("target", "ve", s"${Instant.now().toEpochMilli}").toAbsolutePath
+    val oPath = VeKernelCompiler(s"${getClass.getSimpleName.replaceAllLiterally("$", "")}", veBuildPath)
+        .compile_c(cCode)
+    f(oPath)
+  }
+
+  var inputs: VeColBatch = _
+  var outVectors: List[CVector] = _
+  var lastResult: List[VeColBatch.VeColVector] = _
+
+  def evalFunction()(implicit ctx: OriginalCallingContext): Unit = {
+    withCompiled(func.toCodeLinesWithHeaders.cCode) { path =>
+      import com.nec.spark.SparkCycloneExecutorPlugin.veProcess
+
+      val libRef = veProcess.loadLibrary(path)
+      lastResult = veProcess.execute(libRef, func.name, inputs.cols, outVectors)
+    }
+  }
 }
 
 // implicit conversion
 object VeRDD {
-  implicit def toVectorizedRDD[T](r: RDD[T]): VeRDD[T] = new VeRDD(r)
+  implicit def toVectorizedRDD[T: ClassTag](r: RDD[T]): VeRDD[T] = new VeRDD(r)
 }
