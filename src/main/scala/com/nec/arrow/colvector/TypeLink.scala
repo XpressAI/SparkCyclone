e84462e4dd0f4822562da274d220917c295930db
Benson Ma <q10@users.noreply.github.com>
2022-03-26 01:38:12 -0700

    [colvector] Add conversions for Seq[Option[T]]
    - Add conversions between `Seq[Option[T]]` and `BytePointerColVector`.
    This is intended to be used in VE-based unit tests and for loading data
    inside `MappedVeRDD`s
    
    - Add extensive unit tests to check that the conversions work correctly

--- src/main/scala/com/nec/arrow/colvector/TypeLink.scala
+++ src/main/scala/com/nec/arrow/colvector/TypeLink.scala
@@ -9,7 +9,6 @@ import org.apache.spark.sql.vectorized.ColumnVector
 sealed trait TypeLink {
   type ArrowType <: BaseFixedWidthVector
   def makeArrow(name: String)(implicit allocator: BufferAllocator): ArrowType
-  def transfer(idx: Int, from: ColumnVector, to: ArrowType): Unit
   def veScalarType: VeScalarType
 }
 
@@ -17,12 +16,9 @@ object TypeLink {
   private case object FloatTypeLink extends TypeLink {
     override type ArrowType = Float4Vector
 
-    override def makeArrow(name: String)(implicit allocator: BufferAllocator): ArrowType =
+    override def makeArrow(name: String)(implicit allocator: BufferAllocator): ArrowType = {
       new Float4Vector(name, allocator)
-
-    override def transfer(idx: Int, from: ColumnVector, to: ArrowType): Unit =
-      if (from.isNullAt(idx)) to.setNull(idx)
-      else to.set(idx, from.getFloat(idx))
+    }
 
     override def veScalarType: VeScalarType = VeNullableFloat
   }
@@ -30,12 +26,9 @@ object TypeLink {
   private case object DoubleTypeLink extends TypeLink {
     override type ArrowType = Float8Vector
 
-    override def makeArrow(name: String)(implicit allocator: BufferAllocator): ArrowType =
+    override def makeArrow(name: String)(implicit allocator: BufferAllocator): ArrowType = {
       new Float8Vector(name, allocator)
-
-    override def transfer(idx: Int, from: ColumnVector, to: ArrowType): Unit =
-      if (from.isNullAt(idx)) to.setNull(idx)
-      else to.set(idx, from.getDouble(idx))
+    }
 
     override def veScalarType: VeScalarType = VeNullableDouble
   }
@@ -43,12 +36,9 @@ object TypeLink {
   private case object LongTypeLink extends TypeLink {
     override type ArrowType = BigIntVector
 
-    override def makeArrow(name: String)(implicit allocator: BufferAllocator): ArrowType =
+    override def makeArrow(name: String)(implicit allocator: BufferAllocator): ArrowType = {
       new BigIntVector(name, allocator)
-
-    override def transfer(idx: Int, from: ColumnVector, to: ArrowType): Unit =
-      if (from.isNullAt(idx)) to.setNull(idx)
-      else to.set(idx, from.getLong(idx))
+    }
 
     override def veScalarType: VeScalarType = VeNullableLong
   }
@@ -56,14 +46,9 @@ object TypeLink {
   private case object BooleanTypeLink extends TypeLink {
     override type ArrowType = IntVector
 
-    override def makeArrow(name: String)(implicit allocator: BufferAllocator): ArrowType =
+    override def makeArrow(name: String)(implicit allocator: BufferAllocator): ArrowType = {
       new IntVector(name, allocator)
-
-    override def transfer(idx: Int, from: ColumnVector, to: ArrowType): Unit =
-      if (from.isNullAt(idx)) to.setNull(idx)
-      else {
-        to.set(idx, if (from.getBoolean(idx)) 1 else 0)
-      }
+    }
 
     override def veScalarType: VeScalarType = VeNullableInt
   }
@@ -71,12 +56,9 @@ object TypeLink {
   private case object IntegerTypeLink extends TypeLink {
     override type ArrowType = IntVector
 
-    override def makeArrow(name: String)(implicit allocator: BufferAllocator): ArrowType =
+    override def makeArrow(name: String)(implicit allocator: BufferAllocator): ArrowType = {
       new IntVector(name, allocator)
-
-    override def transfer(idx: Int, from: ColumnVector, to: ArrowType): Unit =
-      if (from.isNullAt(idx)) to.setNull(idx)
-      else to.set(idx, from.getInt(idx))
+    }
 
     override def veScalarType: VeScalarType = VeNullableInt
   }
@@ -84,12 +66,9 @@ object TypeLink {
   private case object ShortTypeLink extends TypeLink {
     override type ArrowType = IntVector
 
-    override def makeArrow(name: String)(implicit allocator: BufferAllocator): ArrowType =
+    override def makeArrow(name: String)(implicit allocator: BufferAllocator): ArrowType = {
       new IntVector(name, allocator)
-
-    override def transfer(idx: Int, from: ColumnVector, to: ArrowType): Unit =
-      if (from.isNullAt(idx)) to.setNull(idx)
-      else to.set(idx, from.getShort(idx).toInt)
+    }
 
     override def veScalarType: VeScalarType = VeNullableShort
   }
@@ -97,26 +76,12 @@ object TypeLink {
   private case object DateTypeLink extends TypeLink {
     override type ArrowType = DateDayVector
 
-    override def makeArrow(name: String)(implicit allocator: BufferAllocator): ArrowType =
+    override def makeArrow(name: String)(implicit allocator: BufferAllocator): ArrowType = {
       new DateDayVector(name, allocator)
-
-    override def transfer(idx: Int, from: ColumnVector, to: ArrowType): Unit =
-      if (from.isNullAt(idx)) to.setNull(idx)
-      else to.set(idx, from.getInt(idx))
+    }
 
     override def veScalarType: VeScalarType = VeNullableInt
   }
-
-  val SparkToArrow: Map[DataType, TypeLink] = Map(
-    BooleanType -> BooleanTypeLink,
-    IntegerType -> IntegerTypeLink,
-    ShortType -> ShortTypeLink,
-    FloatType -> FloatTypeLink,
-    DoubleType -> DoubleTypeLink,
-    LongType -> LongTypeLink,
-    TimestampType -> LongTypeLink,
-    DateType -> DateTypeLink
-  )
 
   val VeToArrow: Map[VeScalarType, TypeLink] = Map(
     VeNullableInt -> IntegerTypeLink,
