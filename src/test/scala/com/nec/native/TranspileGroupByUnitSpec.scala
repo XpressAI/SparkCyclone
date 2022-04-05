36d533bb829e6e76e867acdd0078af0972de8283
Paul Dubs <paul.dubs@gmail.com>
2022-04-05 14:42:02 +0200

    Take care of identity functions and tuple extractions

--- src/test/scala/com/nec/native/TranspileGroupByUnitSpec.scala
+++ src/test/scala/com/nec/native/TranspileGroupByUnitSpec.scala
@@ -4,6 +4,16 @@ import scala.reflect.runtime.universe._
 
 final class TranspileGroupByUnitSpec extends CppTranspilerSpec {
   "CppTranspiler for GroupBy Functions" should {
+    "correctly transpile GroupBy identity" in {
+      val groupByCode = CppTranspiler.transpileGroupBy(reify({ x: Long => x }))
+      println(groupByCode.func.toCodeLinesWithHeaders.cCode)
+    }
+
+    "correctly transpile GroupBy identity of tuple element" in {
+    val groupByCode = CppTranspiler.transpileGroupBy(reify({ x: (Long, Long) => x._1 }))
+    println(groupByCode.func.toCodeLinesWithHeaders.cCode)
+  }
+
     "correctly transpile GroupBy Long -> Long" in {
       val groupByCode = CppTranspiler.transpileGroupBy(reify({ x: Long => x * 2 }))
       println(groupByCode.func.toCodeLinesWithHeaders.cCode)
