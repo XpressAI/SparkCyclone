afe4b439b27d08d3b3f8ab84fb97df651452463e
Paul Dubs <paul.dubs@gmail.com>
2022-04-25 09:08:40 +0200

    Different initialization

--- src/test/scala/com/nec/ve/WithVeProcess.scala
+++ src/test/scala/com/nec/ve/WithVeProcess.scala
@@ -7,23 +7,33 @@ import org.scalatest.{BeforeAndAfterAll, Suite}
 trait WithVeProcess extends BeforeAndAfterAll { this: Suite =>
   implicit def metrics = VeProcessMetrics.noOp
   implicit def source = VeColVectorSource(getClass.getName)
-  implicit def veProcess: VeProcess = VeProcess.WrappingVeo(proc, thr_ctxt, source, VeProcessMetrics.noOp)
+  implicit def veProcess: VeProcess = VeProcess.WrappingVeo(proc_and_ctxt._1, proc_and_ctxt._2, source, VeProcessMetrics.noOp)
 
   private var initialized = false
 
-  private lazy val proc = {
+  private lazy val proc_and_ctxt = {
     initialized = true
-    veo.veo_proc_create(0)
-  }
+    val selectedVeNodeId = 0
+    val proc = veo.veo_proc_create(selectedVeNodeId)
+    val ctxt = veo.veo_context_open(proc)
 
-  private lazy val thr_ctxt = {
-    veo.veo_context_open(proc)
+    require(
+      proc != null,
+      s"Proc could not be allocated for node ${selectedVeNodeId}, got null"
+    )
+    require(
+      ctxt != null,
+      s"Async Context could not be allocated for node ${selectedVeNodeId}, got null"
+    )
+
+    (proc, ctxt)
   }
 
   override protected def afterAll(): Unit = {
     super.afterAll()
     if (initialized) {
-      veo.veo_context_close(thr_ctxt);
+      val (proc, thr_ctxt) = proc_and_ctxt
+      veo.veo_context_close(thr_ctxt)
       veo.veo_proc_destroy(proc)
     }
   }
