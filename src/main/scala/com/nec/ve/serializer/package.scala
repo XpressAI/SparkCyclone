eecb7e5c0484910bfa82dbf1822656bb57732fd2
Eduardo Gonzalez <wm.eddie@gmail.com>
2022-06-09 14:44:45 +0900

    Mark DualBatchOrBytes as Serializable.

--- src/main/scala/com/nec/ve/serializer/package.scala
+++ src/main/scala/com/nec/ve/serializer/package.scala
@@ -1,7 +1,7 @@
 package com.nec.ve
 
-import com.nec.ve.serializer.DualBatchOrBytes.{BytesOnly, ColBatchWrapper}
 import com.nec.colvector.VeColBatch
+import com.nec.ve.serializer.DualBatchOrBytes.{BytesOnly, ColBatchWrapper}
 
 package object serializer {
   val CbTag: Int = 91
@@ -10,12 +10,13 @@ package object serializer {
   val LongTag: Int = 94
   val DoubleTag: Int = 95
 
-  sealed trait DualBatchOrBytes {
+  sealed trait DualBatchOrBytes extends Serializable {
     def fold[T](a: BytesOnly => T, b: VeColBatch => T): T = this match {
       case ColBatchWrapper(veColBatch) => b(veColBatch)
       case bytesOnly: BytesOnly        => a(bytesOnly)
     }
   }
+
   object DualBatchOrBytes {
     final case class ColBatchWrapper(veColBatch: VeColBatch) extends DualBatchOrBytes
     /** Underlying byte buffer may be re-used for performance */
