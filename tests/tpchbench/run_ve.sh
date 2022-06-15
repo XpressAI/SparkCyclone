96ee8b0af8b14e63222ff153f6d0bd2d3e511efd
Marko Nußbaum <marko.nussbaum@maconus.com>
2022-06-15 18:33:33 +0200

    only use 1 executor for easier log inspection

--- tests/tpchbench/run_ve.sh
+++ tests/tpchbench/run_ve.sh
@@ -4,7 +4,7 @@ export SPARK_HOME=/opt/spark
 
 time $SPARK_HOME/bin/spark-submit \
     --master yarn \
-    --num-executors=8 --executor-cores=2 --executor-memory=8G \
+    --num-executors=1 --executor-cores=12 --executor-memory=8G \
     --deploy-mode cluster \
     --name TPC-H_VE_$1 \
     --jars /opt/cyclone/${USER}/spark-cyclone-sql-plugin.jar \
